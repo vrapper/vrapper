d31b81f9cbab4a4f71bc34b8612c3077a1958f2c
Matthias Radig <radig@jroene.de>
2009-02-22 16:20:25 +0000

    remove , from keyword pattern

--- src/core/de/jroene/vrapper/vim/VimConstants.java
+++ src/core/de/jroene/vrapper/vim/VimConstants.java
@@ -15,7 +15,7 @@ public class VimConstants {
     public static final Set<String> WHITESPACE = set(" ", "\t", "\n", "\r");
     public static final String NEWLINE = System.getProperty("line.separator");
     public static final String SPACE = " ";
-    public static final String WORD_CHAR_PATTERN = "[A-Za-z0-9_,]";
+    public static final String WORD_CHAR_PATTERN = "[A-Za-z0-9_]";
 
     private static final <T> Set<T> set(T... content) {
         return Collections.unmodifiableSet(new HashSet<T>(Arrays.asList(content)));
