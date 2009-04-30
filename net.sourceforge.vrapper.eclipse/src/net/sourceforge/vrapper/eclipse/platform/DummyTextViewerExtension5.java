fef151b52ad39955cbb7a312856d44e7813dd9ce
Krzysiek Goj <krzysztof.goj@gmail.com>
2009-04-30 18:16:34 +0000

    Removed error printing on dummy model<->view offset conversions

--- net.sourceforge.vrapper.eclipse/src/net/sourceforge/vrapper/eclipse/platform/DummyTextViewerExtension5.java
+++ net.sourceforge.vrapper.eclipse/src/net/sourceforge/vrapper/eclipse/platform/DummyTextViewerExtension5.java
@@ -1,7 +1,5 @@
 package net.sourceforge.vrapper.eclipse.platform;
 
-import net.sourceforge.vrapper.log.VrapperLog;
-
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewerExtension5;
 
@@ -23,42 +21,34 @@ public class DummyTextViewerExtension5 implements ITextViewerExtension5 {
     }
 
     public int modelLine2WidgetLine(int modelLine) {
-        VrapperLog.error("dummy line conversion");
         return modelLine;
     }
 
     public int modelOffset2WidgetOffset(int modelOffset) {
-        VrapperLog.error("dummy offset conversion");
         return modelOffset;
     }
 
     public IRegion modelRange2WidgetRange(IRegion modelRange) {
-        VrapperLog.error("dummy range conversion");
         return modelRange;
     }
 
     public int widgetLine2ModelLine(int widgetLine) {
-        VrapperLog.error("dummy line conversion");
         return widgetLine;
     }
 
     public int widgetLineOfWidgetOffset(int widgetOffset) {
-        VrapperLog.error("dummy offset conversion");
         return widgetOffset;
     }
 
     public int widgetOffset2ModelOffset(int widgetOffset) {
-        VrapperLog.error("dummy offset conversion");
         return widgetOffset;
     }
 
     public IRegion widgetRange2ModelRange(IRegion widgetRange) {
-        VrapperLog.error("dummy region conversion");
         return widgetRange;
     }
 
     public int widgetlLine2ModelLine(int widgetLine) {
-        VrapperLog.error("dummy line conversion");
         return widgetLine;
     }
 
