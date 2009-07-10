d145489c9e8c560c29d896adc03602f46fae186a
Matthias Radig <radig@jroene.de>
2009-07-10 11:38:29 +0000

    fix linewise change operation

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/ChangeOperation.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/ChangeOperation.java
@@ -1,5 +1,6 @@
 package net.sourceforge.vrapper.vim.commands;
 
+import net.sourceforge.vrapper.utils.ContentType;
 import net.sourceforge.vrapper.vim.EditorAdaptor;
 import net.sourceforge.vrapper.vim.modes.InsertMode;
 
@@ -14,6 +15,9 @@ public class ChangeOperation implements TextOperation {
     public void execute(EditorAdaptor editorAdaptor, int count,
             TextObject textObject) throws CommandExecutionException {
         Command c = new TextOperationTextObjectCommand(DELETE, textObject).withCount(count);
+        if (ContentType.LINES.equals(textObject.getContentType())) {
+            c = new VimCommandSequence(c, new InsertLineCommand(InsertLineCommand.Type.PRE_CURSOR));
+        }
         editorAdaptor.changeMode(InsertMode.NAME, c);
     }
 }
