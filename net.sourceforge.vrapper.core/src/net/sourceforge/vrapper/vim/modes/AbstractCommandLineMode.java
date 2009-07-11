5f9bf2505d3a0947503a6364537175192587670b
Matthias Radig <radig@jroene.de>
2009-07-11 14:36:06 +0000

    fix NPE

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/modes/AbstractCommandLineMode.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/modes/AbstractCommandLineMode.java
@@ -15,6 +15,7 @@ public abstract class AbstractCommandLineMode extends AbstractMode {
 
     public AbstractCommandLineMode(EditorAdaptor editorAdaptor) {
         super(editorAdaptor);
+        parser = createParser();
     }
 
     /**
