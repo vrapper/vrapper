db3688bc17b5c61797ae8c1f741d4a24f3ed40ba
Krzysiek Goj <krzysztof.goj@gmail.com>
2009-12-14 22:51:20 +0000

    StringUtils.repr(String str) method   * Useful for debugging

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/utils/StringUtils.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/utils/StringUtils.java
@@ -2,24 +2,23 @@ package net.sourceforge.vrapper.utils;
 
 import java.util.Arrays;
 
-
 public class StringUtils {
-	public static String multiply(String str, int count) {
-		StringBuilder builder = new StringBuilder();
-		for (int i = 0; i < count; i++) {
+    public static String multiply(String str, int count) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < count; i++) {
             builder.append(str);
         }
-		return builder.toString();
-	}
+        return builder.toString();
+    }
 
-	public static String join(String separator, Object[] items) {
-		return join(separator, Arrays.asList(items));
-	}
-	
+    public static String join(String separator, Object[] items) {
+        return join(separator, Arrays.asList(items));
+    }
+
     public static String join(String separator, Iterable<?> items) {
         StringBuilder builder = new StringBuilder();
         String sep = "";
-        for (Object item: items) {
+        for (Object item : items) {
             builder.append(sep);
             builder.append(item);
             sep = separator;
@@ -35,4 +34,30 @@ public class StringUtils {
         }
         return false;
     }
+
+    public static String repr(String str) {
+        if (str == null)
+            return "null";
+
+        StringBuilder result = new StringBuilder("\"");
+        for (int i = 0; i < str.length(); i++) {
+            char chr = str.charAt(i);
+            switch (chr) {
+                case '\0': result.append("\\0"); break;
+                case '\t': result.append("\\t"); break;
+                case '\r': result.append("\\r"); break;
+                case '\n': result.append("\\n"); break;
+                case '\"': result.append("\\\""); break;
+                case '\\': result.append("\\\\"); break;
+                default:
+                    result.append(isPrintableASCII(chr) ? chr : String.format("\\u%04x", chr));
+            }
+        }
+        return result.append('"').toString();
+    }
+
+    public static boolean isPrintableASCII(char chr) {
+        return ' ' <= chr && chr <= '\u007e';
+    }
+
 }
