755465541020d11c520c289b60346ba86f267be4
Matthias Radig <radig@jroene.de>
2010-04-12 08:38:02 +0000

    update documentation

--- website/features/index.php
+++ website/features/index.php
@@ -1,6 +1,6 @@
 <?php $page="features"; include("../includes/header.html"); ?>
 <div id="features">
-<h3 class="implemented">Implemented as of 0.12.0:</h3>
+<h3 class="implemented">Implemented as of 0.14.0:</h3>
     <ul>
         <li>
             Operators (d, c, y, p, ...
--- website/home/news.php
+++ website/home/news.php
@@ -1,4 +1,23 @@
 <div class="newsbox">
+    <div class="date">2010-04-12</div>
+    <h4>0.14.0 released</h4>
+    <p>Version 0.14.0 has been released. Changes are:</p>
+    <ul>
+        <li>Corrected line-wise visual mode behaviour on empty lines.</li>
+        <li>Yanking text objects moves the cursor on the start of the text object. Use ":set nomoveonyank" for old behaviour.</li>
+        <li>Toggling Vrapper enables/disables Vim-emulation for all open editors.</li>
+        <li>Visual mode operations may be repeated using the dot command.</li>
+        <li>Pasting over selections in visual mode is possible.</li>
+        <li>Join lines positions the cursor between the joined parts.</li>
+        <li>Added paragraph motions and text objects.</li>
+        <li>Bound ctrl-u and ctrl-d to Eclipse actions page-up and page-down.</li>
+        <li>Fixed zz command to work correctly with folded sections and added z., zb, zt, z- commands.</li>
+        <li>gt and gT behave more like in Vim.</li>
+        <li>Pressing caps lock or alt is not interpreted as keystroke anymore, so caps lock and alt can be used in command line mode to input caps and special characters.</li>
+    </ul>
+
+</div>
+<div class="newsbox">
     <div class="date">2009-11-09</div>
     <h4>0.12.0 released</h4>
     <p>Version 0.12.0 has been released. Changes are:</p>
@@ -19,18 +38,5 @@
     <h4>0.10.1 released</h4>
     <p>Version 0.10.1 has been released.</p>
     <p>This release fixes compatibility issues with Eclipse 3.3.</p>
-</div>
-
-<div class="newsbox">
-    <div class="date">2009-09-04</div>
-    <h4>0.10.0 released</h4>
-    <p>Version 0.10.0 has been released. Changes are:</p>
-    <ul>
-        <li>fixed issues with different keyboard layouts</li>
-        <li>bound keys not bound in 0.8: page up, page down, space, return, delete</li>
-        <li>options "ignorecase" and "smartcase" available</li>
-        <li>improved undo behaviour during inserts</li>
-    </ul>
-
 </div>
 
