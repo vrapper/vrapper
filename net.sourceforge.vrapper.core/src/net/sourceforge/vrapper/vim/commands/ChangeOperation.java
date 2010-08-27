0dd88068e9c04c7964403ca25d4f36fb10211bae
Kamil Dworakowski <kamil.dworakowski@youdevise.com>
2010-08-27 16:19:37 +0100

    fix the ctx test
    now the ctx does not enter insert mode if there is no x on the line

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/ChangeOperation.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/ChangeOperation.java
@@ -2,7 +2,6 @@ package net.sourceforge.vrapper.vim.commands;
 
 import net.sourceforge.vrapper.utils.ContentType;
 import net.sourceforge.vrapper.vim.EditorAdaptor;
-import net.sourceforge.vrapper.vim.modes.ExecuteCommandOnEnterHint;
 import net.sourceforge.vrapper.vim.modes.InsertMode;
 
 public class ChangeOperation implements TextOperation {
@@ -21,6 +20,7 @@ public class ChangeOperation implements TextOperation {
         if (ContentType.LINES.equals(textObject.getContentType(editorAdaptor.getConfiguration()))) {
             c = new VimCommandSequence(c, InsertLineCommand.PRE_CURSOR);
         }
-        editorAdaptor.changeMode(InsertMode.NAME, new ExecuteCommandOnEnterHint(c));
+        c.execute(editorAdaptor);
+        editorAdaptor.changeMode(InsertMode.NAME);
     }
 }
