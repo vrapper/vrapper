886647b126849d7aaa189e16d108b6915ef8094a
Krzysztof Goj <krzysztof.goj@erlang-solutions.com>
2010-09-20 08:34:17 +0200

    Trailing whitespace fix

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/keymap/vim/CountingState.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/keymap/vim/CountingState.java
@@ -9,7 +9,7 @@ import net.sourceforge.vrapper.utils.Function;
 import net.sourceforge.vrapper.vim.commands.Counted;
 
 /** Keymap state that implements counting.
- * 
+ *
  * @author Krzysiek Goj
  *
  * @param <T> - counted thing (Command, TextObject, Motion) that is a subject to counting.
@@ -18,9 +18,9 @@ public class CountingState<T extends Counted<T>> implements State<Function<T, T>
 
     @SuppressWarnings("rawtypes")
     static CountingState<?> INSTANCE = new CountingState(0);
-    
+
     private final int count;
-    
+
     private CountingState(int count) {
         this.count = count;
     }
@@ -32,25 +32,25 @@ public class CountingState<T extends Counted<T>> implements State<Function<T, T>
         int lastDigit = Integer.decode(Character.toString(character));
         if (count == 0 && lastDigit == 0)
             return null;
-        
+
         final int newCount = 10 * count + lastDigit;
-        
+
         Function<T, T> applyCount = new Function<T, T>() {
             public T call(T arg) {
                 return arg.withCount(newCount);
             }
         };
-        
+
         return new SimpleTransition<Function<T, T>>(applyCount, new CountingState<T>(newCount));
     }
-    
+
     public State<Function<T, T>> union(State<Function<T, T>> other) {
         throw new UnsupportedOperationException();
     }
-    
+
     @SuppressWarnings("unchecked")
     public static<T extends Counted<T>> State<T> wrap(State<T> inner) {
         return new WrappingState<T>((CountingState<T>) INSTANCE, inner);
     }
-    
+
 }
