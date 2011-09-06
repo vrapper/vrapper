e2011cf58eb09b270551615cc9a5aa7cba953ee2
Ryan Dawe <ryandawe@gmail.com>
2011-09-05 22:35:12 -0400

    removing log message

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/motions/MoveWordRightNoWhitespace.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/motions/MoveWordRightNoWhitespace.java
@@ -1,7 +1,6 @@
 package net.sourceforge.vrapper.vim.commands.motions;
 
 import static net.sourceforge.vrapper.vim.commands.Utils.characterType;
-import net.sourceforge.vrapper.log.VrapperLog;
 
 public class MoveWordRightNoWhitespace extends MoveWordRight {
 	
@@ -13,7 +12,6 @@ public class MoveWordRightNoWhitespace extends MoveWordRight {
     
     @Override
     protected boolean atBoundary(char c1, char c2) {
-    	VrapperLog.info("MoveWordRightNoWhiteSpace("+c1+","+c2+")");
         return characterType(c1) != characterType(c2);
     } 
 
