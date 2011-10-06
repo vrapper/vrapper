2303c940a1c9dbfc1c22e556e593d844b42dd9e5
keforbes <keforbes@gmail.com>
2011-10-05 21:52:40 -0600

    Simplified some code.  Use a parent class rather than storing a delegate.
    Change-Id: I10027b02763991d2800792c724c12cf52bf3eff1

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/motions/MoveBigWORDRightForUpdate.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/motions/MoveBigWORDRightForUpdate.java
@@ -2,44 +2,22 @@ package net.sourceforge.vrapper.vim.commands.motions;
 
 import net.sourceforge.vrapper.utils.Position;
 import net.sourceforge.vrapper.vim.EditorAdaptor;
-import net.sourceforge.vrapper.vim.commands.BorderPolicy;
-import net.sourceforge.vrapper.vim.commands.CommandExecutionException;
 
-public class MoveBigWORDRightForUpdate extends CountAwareMotion {
+public class MoveBigWORDRightForUpdate extends MoveBigWORDRight {
     
     public static final Motion INSTANCE = new MoveBigWORDRightForUpdate(false);
     
-    private CountAwareMotion delegate;
-    
-    private MoveBigWORDRightForUpdate() {}
-    
     private MoveBigWORDRightForUpdate(boolean bailOff) {
-        delegate = new MoveBigWORDRight(bailOff);
-    }
-
-    public BorderPolicy borderPolicy() {
-        return delegate.borderPolicy();
-    }
-
-    public boolean updateStickyColumn() {
-        return delegate.updateStickyColumn();
-    }
-    
-    public int getCount() {
-        return delegate.getCount();
-    }
-    
-    public boolean isJump() {
-        return delegate.isJump();
+    	super(bailOff);
     }
 
     @Override
-    public Position destination(EditorAdaptor editorAdaptor, int count) throws CommandExecutionException {
+    public Position destination(EditorAdaptor editorAdaptor, int count) {
         int originalOffset = editorAdaptor.getPosition().getModelOffset();
-        Position delegatePosition = delegate.destination(editorAdaptor,count);
+        Position parentPosition = super.destination(editorAdaptor,count);
         
-        //differ from the delegate in that we trim the last newline where appropriate
-        int newOffset = MoveWordRightUtils.offsetWithoutLastNewline(originalOffset, delegatePosition.getModelOffset(), editorAdaptor.getModelContent());
+        //differ from the parent in that we trim the last newline where appropriate
+        int newOffset = MoveWordRightUtils.offsetWithoutLastNewline(originalOffset, parentPosition.getModelOffset(), editorAdaptor.getModelContent());
         
         return editorAdaptor.getCursorService().newPositionForModelOffset(newOffset);
     }
