cbad51380983a44674dc039436107a48844d564e
keforbes <keforbes@gmail.com>
2011-12-10 09:24:07 -0700

    Add more unit tests for the yank operation.
    Change-Id: I38bcd54e5ce0b4c21051db8c7c28631cca37b6bd

--- net.sourceforge.vrapper.core.tests/src/net/sourceforge/vrapper/core/tests/cases/NormalModeTests.java
+++ net.sourceforge.vrapper.core.tests/src/net/sourceforge/vrapper/core/tests/cases/NormalModeTests.java
@@ -579,9 +579,19 @@ public class NormalModeTests extends CommandTestCase {
 	@Test
 	public void testCursorAfterYank() {
 	    checkCommand(forKeySeq("yy"),
+	            "first", ' ', "line\nsecond line\nthird line",
+	            "first", ' ', "line\nsecond line\nthird line");
+        assertYanked(ContentType.LINES, "first line\n");
+        
+	    checkCommand(forKeySeq("yy"),
 	            "first line\nsecond", ' ', "line\nthird line",
 	            "first line\nsecond", ' ', "line\nthird line");
         assertYanked(ContentType.LINES, "second line\n");
+        
+	    checkCommand(forKeySeq("yy"),
+	            "first line\nsecond line\nthird", ' ', "line",
+	            "first line\nsecond line\nthird", ' ', "line");
+        assertYanked(ContentType.LINES, "third line\n");
         
 	    checkCommand(forKeySeq("yk"),
 	            "first line\nsecond", ' ', "line\nthird line",
@@ -593,6 +603,26 @@ public class NormalModeTests extends CommandTestCase {
 	            "first line\nsecond", ' ', "line\nthird line");
         assertYanked(ContentType.LINES, "second line\nthird line\n");
         
+	    checkCommand(forKeySeq("y2k"),
+	            "first line\nsecond line\nthird", ' ', "line",
+	            "first", ' ', "line\nsecond line\nthird line");
+        assertYanked(ContentType.LINES, "first line\nsecond line\nthird line\n");
+        
+	    checkCommand(forKeySeq("y2j"),
+	            "first", ' ', "line\nsecond line\nthird line",
+	            "first", ' ', "line\nsecond line\nthird line");
+        assertYanked(ContentType.LINES, "first line\nsecond line\nthird line\n");
+        
+	    checkCommand(forKeySeq("ygg"),
+	            "first line\nsecond", ' ', "line\nthird line",
+	            "first ", 'l', "ine\nsecond line\nthird line");
+        assertYanked(ContentType.LINES, "first line\nsecond line\n");
+        
+	    checkCommand(forKeySeq("yG"),
+	            "first line\nsecond", ' ', "line\nthird line",
+	            "first line\nsecond", ' ', "line\nthird line");
+        assertYanked(ContentType.LINES, "second line\nthird line\n");
+        
 	    checkCommand(forKeySeq("y$"),
 	            "first line\nsecond", ' ', "line\nthird line",
 	            "first line\nsecond", ' ', "line\nthird line");
@@ -600,6 +630,21 @@ public class NormalModeTests extends CommandTestCase {
         
 	    checkCommand(forKeySeq("y0"),
 	            "first line\nsecond", ' ', "line\nthird line",
+	            "first line\n", 's', "econd line\nthird line");
+        assertYanked(ContentType.TEXT, "second");
+        
+	    checkCommand(forKeySeq("yw"),
+	            "first line\nse", 'c', "ond line\nthird line",
+	            "first line\nse", 'c', "ond line\nthird line");
+        assertYanked(ContentType.TEXT, "cond ");
+        
+	    checkCommand(forKeySeq("yb"),
+	            "first line\nse", 'c', "ond line\nthird line",
+	            "first line\n", 's', "econd line\nthird line");
+        assertYanked(ContentType.TEXT, "se");
+        
+	    checkCommand(forKeySeq("yiw"),
+	            "first line\nse", 'c', "ond line\nthird line",
 	            "first line\n", 's', "econd line\nthird line");
         assertYanked(ContentType.TEXT, "second");
 	}
