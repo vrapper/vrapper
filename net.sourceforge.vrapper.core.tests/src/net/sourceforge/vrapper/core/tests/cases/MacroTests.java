9b17c05a52dc675f95f6054be71194d26cd7ea3d
keforbes <keforbes@gmail.com>
2012-02-18 10:28:28 -0700

    Add some more unit tests for named macros and named registers.
    Change-Id: I81aa39e0d8c829a5123f05d82228e156cc7c07e4

--- net.sourceforge.vrapper.core.tests/src/net/sourceforge/vrapper/core/tests/cases/MacroTests.java
+++ net.sourceforge.vrapper.core.tests/src/net/sourceforge/vrapper/core/tests/cases/MacroTests.java
@@ -33,6 +33,28 @@ public class MacroTests extends CommandTestCase {
 				"Ala fo", 'o', " kota");
 	}
 	
+	@Test public void testMultipleMacros() {
+		//define macro 'a'
+		checkCommand(forKeySeq("qacwfoo<ESC>q"),
+				"Ala ",'m', "a kota",
+				"Ala fo", 'o', " kota");
+		
+		//define macro 'b'
+		checkCommand(forKeySeq("qbcwsomething<ESC>q"),
+				"Ala ",'m', "a kota",
+				"Ala somethin", 'g', " kota");
+		
+		//execute macro 'a'
+		checkCommand(forKeySeq("@a"),
+				"Ala ",'m', "a kota",
+				"Ala fo", 'o', " kota");
+		
+		//execute macro 'b'
+		checkCommand(forKeySeq("@b"),
+				"Ala ",'m', "a kota",
+				"Ala somethin", 'g', " kota");
+	}
+	
 	@Test public void testLastMacro() {
 		//define macro
 		checkCommand(forKeySeq("qacwfoo<ESC>q"),
@@ -75,8 +97,7 @@ public class MacroTests extends CommandTestCase {
 				"Ala bla", 'h', " kota");
 	}
 	
-	@Test
-	public void testRegisters() {
+	@Test public void testRegisters() {
 		//yank a word into the "a" register
 		checkCommand(forKeySeq("\"ayw"),
 				"Ala ",'m', "a kota",
@@ -102,4 +123,27 @@ public class MacroTests extends CommandTestCase {
 				"Ala ",'m', "a kota",
 				"Ala mma",' ', "a kota");
 	}
+	
+	@Test public void testMultipleRegisters() {
+		//yank a word into the "a" register
+		checkCommand(forKeySeq("\"ayw"),
+				"Ala ",'m', "a kota",
+				"Ala ",'m', "a kota");
+		
+		//yank a word into the "b" register
+		checkCommand(forKeySeq("\"byw"),
+				"",'A', "la ma kota",
+				"",'A', "la ma kota");
+		
+		//paste from the "a" register
+		checkCommand(forKeySeq("\"ap"),
+				"Ala ",'m', "a kota",
+				"Ala mma",' ', "a kota");
+		
+		//paste from the "b" register
+		checkCommand(forKeySeq("\"bp"),
+				"Ala ",'m', "a kota",
+				"Ala mAla",' ', "a kota");
+	}
+	
 }
