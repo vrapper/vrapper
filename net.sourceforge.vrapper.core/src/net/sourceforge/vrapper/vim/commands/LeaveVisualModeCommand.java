8cb8cf0d2d47123a8bf709e9d625f53e0b58a00f
Richard Quirk <richard.quirk@gmail.com>
2012-06-05 21:18:57 +0200

    Fix wandering visual mode cursor
    This patch fixes the behaviour where pressing vvvv in normal mode
    moved the cursor to the right. This is due to the shift-left introduced
    by the EvilCaret, and it is undone here when leaving visual mode.

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/LeaveVisualModeCommand.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/LeaveVisualModeCommand.java
@@ -3,6 +3,8 @@
  */
 package net.sourceforge.vrapper.vim.commands;
 
+import net.sourceforge.vrapper.platform.CursorService;
+import net.sourceforge.vrapper.utils.Position;
 import net.sourceforge.vrapper.vim.EditorAdaptor;
 import net.sourceforge.vrapper.vim.modes.NormalMode;
 
@@ -18,7 +20,14 @@ public class LeaveVisualModeCommand extends CountIgnoringNonRepeatableCommand {
     }
 
     public static void doIt(EditorAdaptor editorAdaptor) throws CommandExecutionException {
+        Selection sel = editorAdaptor.getSelection();
         editorAdaptor.setSelection(null);
         editorAdaptor.changeMode(NormalMode.NAME);
+        // Fix off-by-one position if selection was left to right
+        if (sel.getStart().getModelOffset() < sel.getEnd().getModelOffset()) {
+            CursorService service = editorAdaptor.getCursorService();
+            Position position = service.getPosition().addModelOffset(-1);
+            service.setPosition(position, true);
+        }
     }
 }
