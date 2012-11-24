77be70a42b7715772666c47dea8783dfd6cb064e
keforbes <keforbes@gmail.com>
2012-11-24 13:48:53 -0700

    Attempt to fix #123.  I had a fix to workaround a different fix. After removing both fixes, everything seems to work now.  I don't know what to believe in anymore.

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/LeaveVisualModeCommand.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/LeaveVisualModeCommand.java
@@ -3,8 +3,6 @@
  */
 package net.sourceforge.vrapper.vim.commands;
 
-import net.sourceforge.vrapper.platform.CursorService;
-import net.sourceforge.vrapper.utils.Position;
 import net.sourceforge.vrapper.vim.EditorAdaptor;
 import net.sourceforge.vrapper.vim.modes.NormalMode;
 
@@ -20,15 +18,7 @@ public class LeaveVisualModeCommand extends CountIgnoringNonRepeatableCommand {
     }
 
     public static void doIt(EditorAdaptor editorAdaptor) throws CommandExecutionException {
-        Selection sel = editorAdaptor.getSelection();
         editorAdaptor.setSelection(null);
         editorAdaptor.changeMode(NormalMode.NAME);
-        // Fix off-by-one position if selection was left to right
-        // (see EvilCaret.java for details)
-        if (sel != null && sel.getStart().getModelOffset() < sel.getEnd().getModelOffset()) {
-            CursorService service = editorAdaptor.getCursorService();
-            Position position = service.getPosition().addModelOffset(-1);
-            service.setPosition(position, true);
-        }
     }
 }
