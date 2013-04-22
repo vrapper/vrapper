6f566057c2536f54d6b03a875a896c1695167c24
keforbes <keforbes@gmail.com>
2013-04-21 19:36:48 -0600

    Working on #125.  I don't think the behavior is quite right but at least it doesn't break Vrapper anymore.

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/VisualMotionCommand.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/VisualMotionCommand.java
@@ -1,5 +1,6 @@
 package net.sourceforge.vrapper.vim.commands;
 
+import net.sourceforge.vrapper.platform.TextContent;
 import net.sourceforge.vrapper.utils.CaretType;
 import net.sourceforge.vrapper.utils.Position;
 import net.sourceforge.vrapper.utils.StartEndTextRange;
@@ -23,6 +24,9 @@ public class VisualMotionCommand extends AbstractVisualMotionCommand {
 			                editorAdaptor.getPosition()));
 			editorAdaptor.setSelection(oldSelection);
 		}
+		else {
+			oldSelection = checkForWindowsNewlines(oldSelection, editorAdaptor);
+		}
 		return oldSelection;
 	}
 
@@ -35,9 +39,10 @@ public class VisualMotionCommand extends AbstractVisualMotionCommand {
 	    if (motion.borderPolicy() == BorderPolicy.INCLUSIVE)
             newSelectionEnd = newSelectionEnd.addViewOffset(1);
 	    
+	    Selection newSelection;
 	    if (editorAdaptor.getConfiguration().get(Options.SELECTION).equals("exclusive")) {
-            editorAdaptor.setSelection(new SimpleSelection(
-	                new StartEndTextRange(oldSelection.getStart(), newSelectionEnd)));
+            newSelection = new SimpleSelection(
+	                new StartEndTextRange(oldSelection.getStart(), newSelectionEnd));
         } else {
             if (newSelectionStart.getModelOffset()-newSelectionEnd.getModelOffset() == 1)
                 newSelectionEnd = newSelectionEnd.addModelOffset(1);
@@ -53,8 +58,46 @@ public class VisualMotionCommand extends AbstractVisualMotionCommand {
 	            CaretType type = newCmp == -1 ? CaretType.LEFT_SHIFTED_RECTANGULAR: CaretType.RECTANGULAR;
 	            editorAdaptor.getCursorService().setCaret(type);
 	        }
-	        editorAdaptor.setSelection(new SimpleSelection(
-	                new StartEndTextRange(newSelectionStart, newSelectionEnd)));
+	        newSelection = new SimpleSelection(
+	                new StartEndTextRange(newSelectionStart, newSelectionEnd));
 	    }
+        newSelection = checkForWindowsNewlines(newSelection, editorAdaptor);
+        editorAdaptor.setSelection(newSelection);
+	}
+	
+	/**
+	 * Windows newlines (\r\n) are causing problems.  If a selection starts or ends
+	 * with only one of those two characters, Eclipse throws an exception.
+	 */
+	private Selection checkForWindowsNewlines(Selection selection, EditorAdaptor editorAdaptor) {
+		TextContent content = editorAdaptor.getModelContent();
+		Position start = selection.getLeftBound();
+		Position end = selection.getRightBound();
+		int startOffset = start.getModelOffset();
+		int endOffset = end.getModelOffset();
+		String text = content.getText(selection);
+		//are we stopping half-way between a multi-byte newline?
+		//(\r\n on Windows, Eclipse can't handle separating them)
+		if(text.startsWith("\n")) {
+			if(startOffset > 0 && content.getText(startOffset-1, 1).equals("\r")) {
+				//include preceding \r
+				start = start.addModelOffset(-1);
+			}
+		}
+		if(text.endsWith("\r")) {
+			if(endOffset < content.getTextLength() && content.getText(endOffset, 1).equals("\n")) {
+				//don't include \r
+				end = end.addModelOffset(-1);
+			}
+		}
+		
+		if(selection.isReversed()) {
+			return new SimpleSelection(
+				new StartEndTextRange(end, start));
+		}
+		else {
+			return new SimpleSelection(
+				new StartEndTextRange(start, end));
+		}
 	}
 }
