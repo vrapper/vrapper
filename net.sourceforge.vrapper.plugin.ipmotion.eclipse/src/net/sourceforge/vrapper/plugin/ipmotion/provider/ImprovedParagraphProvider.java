9c1676a3744bc58683ce05ee303363449e181254
dhleong <falcone88@gmail.com>
2013-05-05 15:14:56 -0400

    d/c/y use the new improved motion! ipmotion.vim doesn't...? I like it...

--- net.sourceforge.vrapper.plugin.ipmotion.eclipse/src/net/sourceforge/vrapper/plugin/ipmotion/provider/ImprovedParagraphProvider.java
+++ net.sourceforge.vrapper.plugin.ipmotion.eclipse/src/net/sourceforge/vrapper/plugin/ipmotion/provider/ImprovedParagraphProvider.java
@@ -1,13 +1,21 @@
 package net.sourceforge.vrapper.plugin.ipmotion.provider;
 
+import static net.sourceforge.vrapper.keymap.StateUtils.union;
 import static net.sourceforge.vrapper.keymap.vim.ConstructorWrappers.leafBind;
+import static net.sourceforge.vrapper.keymap.vim.ConstructorWrappers.operatorCmds;
 import static net.sourceforge.vrapper.keymap.vim.ConstructorWrappers.state;
 import net.sourceforge.vrapper.eclipse.keymap.AbstractEclipseSpecificStateProvider;
 import net.sourceforge.vrapper.keymap.State;
 import net.sourceforge.vrapper.keymap.vim.GoThereState;
+import net.sourceforge.vrapper.keymap.vim.TextObjectState;
 import net.sourceforge.vrapper.platform.PlatformSpecificStateProvider;
 import net.sourceforge.vrapper.plugin.ipmotion.commands.motions.ImprovedParagraphMotion;
+import net.sourceforge.vrapper.vim.commands.ChangeOperation;
 import net.sourceforge.vrapper.vim.commands.Command;
+import net.sourceforge.vrapper.vim.commands.DeleteOperation;
+import net.sourceforge.vrapper.vim.commands.TextObject;
+import net.sourceforge.vrapper.vim.commands.TextOperation;
+import net.sourceforge.vrapper.vim.commands.YankOperation;
 import net.sourceforge.vrapper.vim.commands.motions.Motion;
 
 public class ImprovedParagraphProvider extends AbstractEclipseSpecificStateProvider {
@@ -27,8 +35,18 @@ public class ImprovedParagraphProvider extends AbstractEclipseSpecificStateProvi
         final State<Motion> ipMotions = state(
                 leafBind('{', paragraphBackward),
                 leafBind('}', paragraphForward));
-//        State<TextObject> ipObjects = new TextObjectState(ipMotions);
-//        ipObjects = CountingState.wrap(ipObjects);
-        return new GoThereState(ipMotions);
+        final State<Command> motionCommands = new GoThereState(ipMotions);
+        
+        final State<TextObject> ipObjects = new TextObjectState(ipMotions);
+        
+        final TextOperation delete = DeleteOperation.INSTANCE;
+        final TextOperation change = ChangeOperation.INSTANCE;
+        final TextOperation yank   = YankOperation.INSTANCE;
+        
+        return union(
+                operatorCmds('d', delete, ipObjects),
+                operatorCmds('c', change, ipObjects),
+                operatorCmds('y', yank,   ipObjects),
+                motionCommands);
     }
 }
