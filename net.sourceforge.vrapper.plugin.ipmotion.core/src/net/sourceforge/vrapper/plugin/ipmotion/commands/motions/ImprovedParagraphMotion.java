a994388b2aa94ec65ceb93c8afad6c0a87747486
dhleong <falcone88@gmail.com>
2013-05-05 14:58:46 -0400

    Tweak IPMotion to use regex, mirroring the script, for future expansion

--- net.sourceforge.vrapper.plugin.ipmotion.core/src/net/sourceforge/vrapper/plugin/ipmotion/commands/motions/ImprovedParagraphMotion.java
+++ net.sourceforge.vrapper.plugin.ipmotion.core/src/net/sourceforge/vrapper/plugin/ipmotion/commands/motions/ImprovedParagraphMotion.java
@@ -1,5 +1,7 @@
 package net.sourceforge.vrapper.plugin.ipmotion.commands.motions;
 
+import java.util.regex.Pattern;
+
 import net.sourceforge.vrapper.platform.TextContent;
 import net.sourceforge.vrapper.utils.LineInformation;
 import net.sourceforge.vrapper.vim.commands.motions.ParagraphMotion;
@@ -8,16 +10,44 @@ public class ImprovedParagraphMotion extends ParagraphMotion {
     
     public static final ImprovedParagraphMotion FORWARD = new ImprovedParagraphMotion(true);
     public static final ImprovedParagraphMotion BACKWARD = new ImprovedParagraphMotion(false);
+    
+    /** From original ipmotion.vom:
+     * 
+     *  The global definition of paragraph boundary.
+     *  Default value is "\s*$".
+     *  It can be changed in .vimrc or anytime. Defining
+     *  b:ip_boundary will override this setting.
+     *
+     *  Example:
+     *  :let g:ip_boundary = '"\?\s*$'
+     *  Setting that will make empty lines, and lines only
+     *  contains '"' as boundaries.
+     *
+     *  Note that there is no need adding a "^" sign at the
+     *  beginning. It is enforced by the script.
+     * 
+     */
+    private final String gIpBoundary = "\\s*$";
+    
+    /**
+     * Built from gIpBoundary; used to determine line emptiness.
+     *  If vrapper supports script variables, the behavior could
+     *  be customized....
+     */
+    private final Pattern mIpBoundary;
 
     protected ImprovedParagraphMotion(final boolean moveForward) {
         super(moveForward);
+        
+        mIpBoundary = Pattern.compile("^(" + gIpBoundary +")");
     }
 
     @Override
     protected boolean doesLineEmptinessEqual(final boolean equalWhat, final TextContent content, final int lineNo) {
         final LineInformation info = content.getLineInformation(lineNo);
         final String line = content.getText(info.getBeginOffset(), info.getLength());
-        final boolean isEmpty = line.trim().length() == 0;
+        final boolean isEmpty = mIpBoundary.matcher(line).matches();
+//        final boolean isEmpty = Pattern.matches("^(" + gIpBoundary + ")", line); // for testing, with hotswap
         return isEmpty == equalWhat;
     }
     
