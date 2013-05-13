bceaa688ee9867268f97fef4674b8dc4416c0122
albertdev <albert.devs@gmail.com>
2013-05-13 22:53:53 +0200

    ChangeDelimiterCommand can now change to ReplaceDelimiterMode.
    In this case the replace only takes place when the ReplaceDelimiterMode has exited.

--- net.sourceforge.vrapper.plugin.surround.core/src/net/sourceforge/vrapper/plugin/surround/commands/ChangeDelimiterCommand.java
+++ net.sourceforge.vrapper.plugin.surround.core/src/net/sourceforge/vrapper/plugin/surround/commands/ChangeDelimiterCommand.java
@@ -1,6 +1,8 @@
 package net.sourceforge.vrapper.plugin.surround.commands;
 
 import net.sourceforge.vrapper.platform.TextContent;
+import net.sourceforge.vrapper.plugin.surround.mode.ReplaceDelimiterMode;
+import net.sourceforge.vrapper.plugin.surround.state.AbstractDynamicDelimiterHolder;
 import net.sourceforge.vrapper.plugin.surround.state.DelimiterHolder;
 import net.sourceforge.vrapper.utils.TextRange;
 import net.sourceforge.vrapper.vim.EditorAdaptor;
@@ -11,25 +13,31 @@ import net.sourceforge.vrapper.vim.commands.DelimitedText;
 public class ChangeDelimiterCommand extends CountAwareCommand {
 
     private final DelimitedText delimitedText;
-    private final DelimiterHolder delimiters;
+    private final DelimiterHolder replacement;
 
-    public ChangeDelimiterCommand(DelimitedText delimitedText, DelimiterHolder delimiters) {
+    public ChangeDelimiterCommand(DelimitedText delimitedText, DelimiterHolder replacement) {
         this.delimitedText = delimitedText;
-        this.delimiters = delimiters;
+        this.replacement = replacement;
     }
 
     @Override
     public void execute(EditorAdaptor editorAdaptor, int count) throws CommandExecutionException {
-        try {
-            editorAdaptor.getHistory().beginCompoundChange();
-            TextRange rightRange = delimitedText.rightDelimiter(editorAdaptor, count);
-            TextRange leftRange = delimitedText.leftDelimiter(editorAdaptor, count);
-            change(editorAdaptor, rightRange, delimiters.getRight());
-            change(editorAdaptor, leftRange, delimiters.getLeft());
-            // TODO: option to turn it off?
-            editorAdaptor.getCursorService().setPosition(leftRange.getLeftBound(), true);
-        } finally {
-            editorAdaptor.getHistory().endCompoundChange();
+        if (replacement instanceof AbstractDynamicDelimiterHolder) {
+            // Update delimiters using this extension mode
+            ReplaceDelimiterMode.switchMode(editorAdaptor, delimitedText,
+                    (AbstractDynamicDelimiterHolder) replacement);
+        } else {
+            try {
+                editorAdaptor.getHistory().beginCompoundChange();
+                TextRange rightRange = delimitedText.rightDelimiter(editorAdaptor, count);
+                TextRange leftRange = delimitedText.leftDelimiter(editorAdaptor, count);
+                change(editorAdaptor, rightRange, replacement.getRight());
+                change(editorAdaptor, leftRange, replacement.getLeft());
+                // TODO: option to turn it off?
+                editorAdaptor.getCursorService().setPosition(leftRange.getLeftBound(), true);
+            } finally {
+                editorAdaptor.getHistory().endCompoundChange();
+            }
         }
     }
     
