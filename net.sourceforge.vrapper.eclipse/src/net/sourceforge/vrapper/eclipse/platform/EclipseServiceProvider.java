182f8bd936044ec0c55da1d58c49749c11a4831c
igrekster <igrek@gmx.com>
2013-05-19 08:41:40 +1000

    Allow eclipseaction with parameters.
    eclipseaction sc org.eclipse.ui.views.showView(org.eclipse.ui.views.showView.viewId=org.eclipse.ui.console.ConsoleView)

--- net.sourceforge.vrapper.eclipse/src/net/sourceforge/vrapper/eclipse/platform/EclipseServiceProvider.java
+++ net.sourceforge.vrapper.eclipse/src/net/sourceforge/vrapper/eclipse/platform/EclipseServiceProvider.java
@@ -2,21 +2,27 @@ package net.sourceforge.vrapper.eclipse.platform;
 
 import net.sourceforge.vrapper.platform.ServiceProvider;
 
+import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.handlers.IHandlerService;
 import org.eclipse.ui.texteditor.AbstractTextEditor;
 
 public class EclipseServiceProvider implements ServiceProvider {
 
     private final IHandlerService handlerService;
+    private final ICommandService commandService;
 
     public EclipseServiceProvider(AbstractTextEditor abstractTextEditor) {
         handlerService = (IHandlerService) abstractTextEditor.getSite().getService(IHandlerService.class);
+        commandService = (ICommandService) abstractTextEditor.getSite().getService(ICommandService.class);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getService(Class<T> serviceClass) {
         if (IHandlerService.class.equals(serviceClass)) {
             return (T) handlerService;
+        }
+        if (ICommandService.class.equals(serviceClass)) {
+            return (T) commandService;
         }
         return null;
     }
