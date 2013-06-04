e13210dd4fd7bb43e4678546146f9ed9f1f32b5f
igrekster <igrek@gmx.com>
2013-06-05 07:59:38 +1000

    Command line editing improvements.
    - Added register pasting (C-R).
    - Added HOME/END movement commands.
    - History is not polluted if command is executed through
      a mapping (nmap X :foobar<CR>).
    - Added C-V for pasting from clipboard.
    - Added C-U - delete to the beginning of line.

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/modes/commandline/AbstractCommandParser.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/modes/commandline/AbstractCommandParser.java
@@ -3,13 +3,19 @@ package net.sourceforge.vrapper.vim.modes.commandline;
 import static net.sourceforge.vrapper.keymap.vim.ConstructorWrappers.ctrlKey;
 import static net.sourceforge.vrapper.keymap.vim.ConstructorWrappers.key;
 import static net.sourceforge.vrapper.vim.commands.Utils.characterType;
+
+import java.util.HashMap;
+
 import net.sourceforge.vrapper.keymap.KeyStroke;
 import net.sourceforge.vrapper.keymap.SpecialKey;
+import net.sourceforge.vrapper.keymap.vim.SimpleKeyStroke;
 import net.sourceforge.vrapper.platform.Platform;
+import net.sourceforge.vrapper.utils.VimUtils;
 import net.sourceforge.vrapper.vim.EditorAdaptor;
 import net.sourceforge.vrapper.vim.commands.Command;
 import net.sourceforge.vrapper.vim.modes.ExecuteCommandHint;
 import net.sourceforge.vrapper.vim.modes.NormalMode;
+import net.sourceforge.vrapper.vim.register.DefaultRegisterManager;
 import net.sourceforge.vrapper.vim.register.RegisterManager;
 
 /**
@@ -24,12 +30,17 @@ public abstract class AbstractCommandParser {
     protected static final KeyStroke KEY_ESCAPE  = key(SpecialKey.ESC);
     protected static final KeyStroke KEY_CTRL_C  = ctrlKey('c');
     protected static final KeyStroke KEY_CTRL_W  = ctrlKey('w');
+    protected static final KeyStroke KEY_CTRL_R  = ctrlKey('r');
+    protected static final KeyStroke KEY_CTRL_U  = ctrlKey('u');
+    protected static final KeyStroke KEY_CTRL_V  = ctrlKey('v');
     protected static final KeyStroke KEY_BACKSP  = key(SpecialKey.BACKSPACE);
     protected static final KeyStroke KEY_DELETE  = key(SpecialKey.DELETE);
     protected static final KeyStroke KEY_UP      = key(SpecialKey.ARROW_UP);
     protected static final KeyStroke KEY_DOWN    = key(SpecialKey.ARROW_DOWN);
     protected static final KeyStroke KEY_RIGHT   = key(SpecialKey.ARROW_RIGHT);
     protected static final KeyStroke KEY_LEFT    = key(SpecialKey.ARROW_LEFT);
+    protected static final KeyStroke KEY_HOME    = key(SpecialKey.HOME);
+    protected static final KeyStroke KEY_END     = key(SpecialKey.END);
     protected static final SpecialKey KEY_TAB    = SpecialKey.TAB;
     protected static final SpecialKey KEY_INSERT = SpecialKey.INSERT;
     protected final StringBuffer buffer;
@@ -39,6 +50,64 @@ public abstract class AbstractCommandParser {
     private boolean modified;
     private int position;
     private boolean isFromVisual = false;
+    protected boolean pasteRegister = false;
+    private boolean isCommandLineHistoryEnabled = true;
+    
+    private interface KeyHandler {
+        public void handleKey();
+    }
+    private HashMap<KeyStroke, KeyHandler> editMap = new HashMap<KeyStroke, KeyHandler>();
+    {
+        editMap.put(KEY_UP, new KeyHandler() { public void handleKey() {
+            if (modified)
+                history.setTemp(getCommand());
+            String previous = history.getPrevious();
+            setCommandFromHistory(previous);
+        }});
+        editMap.put(KEY_DOWN, new KeyHandler() { public void handleKey() {
+            if (modified)
+                history.setTemp(getCommand());
+            String next = history.getNext();
+            setCommandFromHistory(next);
+        }});
+        editMap.put(KEY_BACKSP, new KeyHandler() { public void handleKey() {
+            if (position > 1) {
+                buffer.replace(position - 1, position, "");
+                modified = true;
+                position--;
+            }
+        }});
+        editMap.put(KEY_LEFT, new KeyHandler() { public void handleKey() {
+            position = position > 1 ? position - 1 : position;
+        }});
+        editMap.put(KEY_RIGHT, new KeyHandler() { public void handleKey() {
+            position = position != buffer.length() ? position + 1 : buffer.length();
+        }});
+        editMap.put(KEY_DELETE, new KeyHandler() { public void handleKey() {
+            buffer.replace(position, position + 1, "");
+            modified = true;
+        }});
+        editMap.put(KEY_CTRL_R, new KeyHandler() { public void handleKey() {
+            pasteRegister = true;
+            buffer.insert(position, '"');
+            modified = true;
+        }});
+        editMap.put(KEY_HOME, new KeyHandler() { public void handleKey() {
+            position = 1;
+        }});
+        editMap.put(KEY_END, new KeyHandler() { public void handleKey() {
+            position = buffer.length();
+        }});
+        editMap.put(KEY_CTRL_W, new KeyHandler() { public void handleKey() {
+            deleteWordBack();
+        }});
+        editMap.put(KEY_CTRL_U, new KeyHandler() { public void handleKey() {
+            buffer.replace(1, position, "");
+            modified = true;
+            position = 1;
+        }});
+    }
+
 
     public AbstractCommandParser(EditorAdaptor vim) {
         this.editor = vim;
@@ -56,79 +125,49 @@ public abstract class AbstractCommandParser {
      * Up/down arrows handle command line history.
      */
     public void type(KeyStroke e) {
-    	Command c = null;
-        if (e.equals(KEY_RETURN)) {
-            c = parseAndExecute();
-        } else if (e.equals(KEY_BACKSP)) {
-            // only delete if not trying to delete the initial command character from under a command
-            if (position > 1 || buffer.length() == 1) {
-                buffer.replace(position - 1, position, "");
-                position--;
-                modified = true;
-            }
-        } else if (e.equals(KEY_DELETE)) {
-            if (position < buffer.length()) {
-                buffer.replace(position, position+1, "");
-                modified = true;
-            }
-        //use Shift+Insert for paste since Eclipse uses Ctrl+V
-        //(Eclipse uses Shift+Insert too but I think it's acceptable to unbind that one)
-        } else if (e.getSpecialKey() == KEY_INSERT && e.withShiftKey()) {
-            String text = editor.getRegisterManager().getRegister(
-                    RegisterManager.REGISTER_NAME_CLIPBOARD).getContent().getText();
-            text = text.replace('\n', ' ').replace('\r', ' ');
-            buffer.append(text);
-            position = buffer.length();
-        } else if (e.equals(KEY_CTRL_W)) {
-        	deleteWordBack();
-        } else if (e.getSpecialKey() == KEY_TAB) { //tab-completion for filenames
-        	String prefix = null;
-        	if(buffer.toString().startsWith(":e ")) {
-	        	//command starts with ":e " so filename starts at index 3
-	        	prefix = buffer.substring(3);
-        		prefix = tabComplete.getNextMatch(prefix, false, false, e.withShiftKey());
-        		buffer.setLength(0);
-        		buffer.append(":e " + prefix);
-        		position = buffer.length();
-        	}
-        	else if(buffer.toString().startsWith(":find ") ||
-        			buffer.toString().startsWith(":tabf ") ) {
-	        	//command starts with ":find " so filename starts at index 6
-	        	prefix = buffer.substring(6);
-        		prefix = tabComplete.getNextMatch(prefix, true, false, e.withShiftKey());
-        		buffer.setLength(6);
-        		buffer.append(prefix);
-        		position = buffer.length();
-        	}
-        	else if(buffer.toString().startsWith(":cd ")) {
-	        	//command starts with ":cd " so filename starts at index 4
-	        	prefix = buffer.substring(4);
-        		prefix = tabComplete.getNextMatch(prefix, false, true, e.withShiftKey());
-        		buffer.setLength(0);
-        		buffer.append(":cd " + prefix);
-        		position = buffer.length();
-        	}
-        	//else, user hit TAB for no reason
-        } else if (e.equals(KEY_UP)) {
-            if (modified)
-                history.setTemp(getCommand());
-            String previous = history.getPrevious();
-            setCommandFromHistory(previous);
-        } else if (e.equals(KEY_DOWN)) {
-            if (modified)
-                history.setTemp(getCommand());
-            String next = history.getNext();
-            setCommandFromHistory(next);
-        } else if (e.equals(KEY_RIGHT)) {
-            if (position < buffer.length())
-            	position++;
-        } else if (e.equals(KEY_LEFT)) {
-            if (position > 1)
-            	position--;
-        } else {
-            buffer.insert(position, e.getCharacter());
-            position++;
+        Command c = null;
+        if (pasteRegister) {
+            // remove " placed by C-R
+            buffer.replace(position, position + 1, "");
             modified = true;
+        }
+        KeyHandler mappedHandler = editMap.get(e);
+        if (mappedHandler != null) {
+            mappedHandler.handleKey();
+        } else {
+            if (e.equals(KEY_RETURN)) {
+                // Disable history if executed through a mapping (nmap X :foobar<CR>).
+                if (isHistoryEnabled() && e.isVirtual()) {
+                    setHistoryEnabled(false);
+                }
+                c = parseAndExecute();
+            } else {
+                if (e.equals(KEY_CTRL_V) || (e.getSpecialKey() == KEY_INSERT && e.withShiftKey())) {
+                    pasteRegister = true;
+                    e = new SimpleKeyStroke(DefaultRegisterManager.REGISTER_NAME_CLIPBOARD.charAt(0));
+                } else {
+                    if (e.getSpecialKey() == KEY_TAB) { //tab-completion for filenames
+                        completeArgument(e);
+                        pasteRegister = false;
+                        return;
+                    }
+                }
+            }
+            if (pasteRegister) {
+                String text = editor.getRegisterManager().getRegister(Character.toString(e.getCharacter())).getContent().getText();
+                text = VimUtils.stripLastNewline(text);
+                text = text.replace("\r\n", " ").replace('\n', ' ').replace('\r', ' ');
+                buffer.insert(position, text);
+                position += text.length();
+                pasteRegister = false;
+                modified = true;
+            } else {
+                buffer.insert(position++, e.getCharacter());
+                modified = true;
+            }
+        }
+        if (!e.equals(KEY_CTRL_R) && pasteRegister) {
+            pasteRegister = false;
         }
 
         if (buffer.length() == 0 || e.equals(KEY_RETURN)
@@ -137,6 +176,34 @@ public abstract class AbstractCommandParser {
 	            editor.changeModeSafely(NormalMode.NAME, new ExecuteCommandHint.OnEnter(c));
             else
 	            editor.changeModeSafely(NormalMode.NAME);
+        }
+    }
+    
+    private void completeArgument(KeyStroke e) {
+        int cmdLen = 0;
+        boolean paths = false;
+        boolean dirsOnly = false;
+        if (buffer.toString().startsWith(":e ")) {
+            cmdLen = 3;
+        } else {
+            if(buffer.toString().startsWith(":find ") ||
+                    buffer.toString().startsWith(":tabf ") ) {
+                cmdLen = 6;
+                paths = true;
+            } else {
+                if(buffer.toString().startsWith(":cd ")) {
+                    cmdLen = 4;
+                    dirsOnly = true;
+                }
+            }
+        }
+        if (cmdLen > 0) {
+            String cmd = buffer.substring(0, cmdLen);
+            String prefix = buffer.substring(cmdLen);
+            prefix = tabComplete.getNextMatch(prefix, paths, dirsOnly, e.withShiftKey());
+            setBuffer(cmd + prefix);
+        } else {
+            // user hit TAB for no reason
         }
     }
 
@@ -158,8 +225,8 @@ public abstract class AbstractCommandParser {
     		//this line was stolen from MoveWordLeft because
     		//I can't call that class with arbitrary text
     		if(!Character.isWhitespace(c2) && characterType(c1) != characterType(c2)) {
-    			buffer.setLength(offset);
-    			position = buffer.length();
+    			position = offset;
+    			clearBufferFromPosition();
     			return;
     		}
     		offset--;
@@ -173,6 +240,25 @@ public abstract class AbstractCommandParser {
 
     public String getBuffer() {
         return buffer.toString();
+    }
+    
+    public void setBuffer(String s) {
+        buffer.replace(0, buffer.length(), s);
+        position = buffer.length();
+        modified = true;
+    }
+
+    public void clearBufferFromPosition() {
+        buffer.replace(position, buffer.length(), "");
+        modified = true;
+    }
+    
+    public boolean isHistoryEnabled() {
+        return isCommandLineHistoryEnabled;
+    }
+
+    public void setHistoryEnabled(boolean isCommandLineHistoryEnabled) {
+        this.isCommandLineHistoryEnabled = isCommandLineHistoryEnabled;
     }
 
     /**
@@ -192,9 +278,12 @@ public abstract class AbstractCommandParser {
 
     private Command parseAndExecute() {
         String first = buffer.substring(0,1);
-        String command = getCommand();
-        history.append(command);
-        return parseAndExecute(first, command);
+        String c = getCommand();
+        if (isHistoryEnabled()) {
+            history.append(c);
+            setHistoryEnabled(false);
+        }
+        return parseAndExecute(first, c);
     }
 
     public boolean isFromVisual() {
