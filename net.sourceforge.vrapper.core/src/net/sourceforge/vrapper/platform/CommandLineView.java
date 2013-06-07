653c9a9fb046c169b2424da8c4c92075e48a50ea
albertdev <albert.devs@gmail.com>
2013-06-07 09:20:27 +0200

    Work on #218: Add new interface for command line view.

---
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/platform/CommandLineView.java
@@ -0,0 +1,29 @@
+package net.sourceforge.vrapper.platform;
+
+/**
+ * Implementations of this interface will draw the command line view.
+ * <p>
+ * The command line can be user-editable (select, cut / copy / paste) if it was created that
+ * way.
+ */
+public interface CommandLineView {
+    public void setPrompt(String prompt);
+
+    public void setContents(String contents);
+
+    /**
+     * Returns the contents of the textbox excluding prompt characters.
+     * This can be called after closing the window.
+     */
+    public String getContents();
+
+    /**
+     * Returns the contents of the textbox including the prompt characters.
+     * This can be called after closing the window.
+     */
+    public String getFullContents();
+
+    public void append(String characters);
+
+    public void close();
+}
