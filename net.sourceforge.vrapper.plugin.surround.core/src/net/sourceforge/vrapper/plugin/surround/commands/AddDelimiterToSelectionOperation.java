5fd09164f901240c32e0bbe3ea9a1ac2a2bb39e3
albertdev <albert.devs@gmail.com>
2013-07-20 17:22:34 +0200

    Work on #251: implement line wise surround
    Linewise surround mode should be completed. History locking is
    necessary because the line additions, indentation and delimiter
    changes would result in multiple changes.
    
    Prepared code for dynamic delimiter support, still needs to be handled
    in the *State classes.

--- net.sourceforge.vrapper.plugin.surround.core/src/net/sourceforge/vrapper/plugin/surround/commands/AddDelimiterToSelectionOperation.java
+++ net.sourceforge.vrapper.plugin.surround.core/src/net/sourceforge/vrapper/plugin/surround/commands/AddDelimiterToSelectionOperation.java
@@ -1,51 +1,111 @@
 package net.sourceforge.vrapper.plugin.surround.commands;
 
+import net.sourceforge.vrapper.platform.CursorService;
+import net.sourceforge.vrapper.platform.TextContent;
+import net.sourceforge.vrapper.plugin.surround.state.AbstractDynamicDelimiterHolder;
 import net.sourceforge.vrapper.plugin.surround.state.DelimiterHolder;
 import net.sourceforge.vrapper.utils.ContentType;
+import net.sourceforge.vrapper.utils.LineInformation;
+import net.sourceforge.vrapper.utils.Position;
+import net.sourceforge.vrapper.utils.StartEndTextRange;
 import net.sourceforge.vrapper.utils.TextRange;
 import net.sourceforge.vrapper.vim.EditorAdaptor;
 import net.sourceforge.vrapper.vim.commands.BlockWiseSelection;
 import net.sourceforge.vrapper.vim.commands.BlockWiseSelection.TextBlock;
 import net.sourceforge.vrapper.vim.commands.Command;
 import net.sourceforge.vrapper.vim.commands.CommandExecutionException;
+import net.sourceforge.vrapper.vim.commands.InsertLineCommand;
+import net.sourceforge.vrapper.vim.commands.LineWiseSelection;
+import net.sourceforge.vrapper.vim.commands.SimpleSelection;
 import net.sourceforge.vrapper.vim.commands.TextObject;
 import net.sourceforge.vrapper.vim.commands.TextOperation;
 
-public class AddDelimiterToSelectionOperation implements TextOperation {
+public class AddDelimiterToSelectionOperation implements TextOperation, DelimiterChangedListener {
 
-    private DelimiterHolder delimiters;
+    private DelimiterHolder replacement;
     private TextOperation indentOperation;
 
     public AddDelimiterToSelectionOperation(DelimiterHolder delimiters, TextOperation indentOperation) {
         this.indentOperation = indentOperation;
-        this.delimiters = delimiters;
+
+        //If the delimiter is dynamic, it must be updated through delimiterChanged(..)
+        if ( ! (delimiters instanceof AbstractDynamicDelimiterHolder)) {
+            this.replacement = delimiters;
+        }
     }
 
     @Override
     public void execute(EditorAdaptor editorAdaptor, int count, TextObject selection)
             throws CommandExecutionException {
-        if (selection.getContentType(editorAdaptor.getConfiguration())
-                .equals(ContentType.LINES)) {
-            surroundLines(editorAdaptor, selection);
+        try {
+            editorAdaptor.getHistory().beginCompoundChange();
+            editorAdaptor.getHistory().lock("surroundvisual");
+            
+            if (replacement == null) {
+                throw new CommandExecutionException("Dynamic delimiter was never updated!");
+            }
+            if (selection.getContentType(editorAdaptor.getConfiguration())
+                    .equals(ContentType.LINES)) {
 
-        } else if (selection.getContentType(editorAdaptor.getConfiguration())
-                .equals(ContentType.TEXT_RECTANGLE)) {
-            surroundLinesInBlock(editorAdaptor, selection);
+                surroundLines(editorAdaptor, selection);
 
-        } else {
-            new ChangeDelimiterCommand(new NotYetDelimitedTextObject(selection), delimiters)
-                    .execute(editorAdaptor);
+            } else if (selection.getContentType(editorAdaptor.getConfiguration())
+                    .equals(ContentType.TEXT_RECTANGLE)) {
+
+                surroundLinesInBlock(editorAdaptor, selection);
+
+            } else {
+                ChangeDelimiterCommand.doIt(editorAdaptor, 0, new NotYetDelimitedTextObject(selection),
+                        replacement);
+            }
+        } finally {
+            editorAdaptor.getHistory().unlock("surroundvisual");
+            editorAdaptor.getHistory().endCompoundChange();
         }
     }
 
     private void surroundLines(EditorAdaptor editorAdaptor, TextObject selection)
             throws CommandExecutionException {
+        TextContent content = editorAdaptor.getModelContent();
+        TextRange range = selection.getRegion(editorAdaptor, 0);
+        CursorService cursor = editorAdaptor.getCursorService();
+
+        Position startContents = range.getLeftBound();
+        Position endContents = range.getRightBound().addModelOffset(-1); //Exclusive bound
+
+        //Use marks to keep correct Positions
+        cursor.setMark(CursorService.LAST_CHANGE_START, startContents);
+        cursor.setMark(CursorService.LAST_CHANGE_END, endContents);
+
+        //Create new lines before and after selection
+        LineInformation startLine = content.getLineInformationOfOffset(startContents.getModelOffset());
+        InsertLineCommand.doIt(editorAdaptor, InsertLineCommand.Type.PRE_CURSOR, startLine);
+        Position startSurround = cursor.getPosition(); // End of first inserted line
+
+        startContents = cursor.getMark(CursorService.LAST_CHANGE_START);
+        endContents = cursor.getMark(CursorService.LAST_CHANGE_END);
+
+        LineInformation endLine = content.getLineInformationOfOffset(endContents.getModelOffset());
+        InsertLineCommand.doIt(editorAdaptor, InsertLineCommand.Type.POST_CURSOR, endLine);
+        Position endSurround = cursor.getPosition(); // End of last insterted line
+        
+        //Create new selection, offsets got shifted due to first inserted line.
+        selection = new LineWiseSelection(editorAdaptor, startContents, endContents);
+
+        //Save position in mark to keep it updated, indentation would otherwise invalidate them.
+        cursor.setMark(CursorService.LAST_CHANGE_START, startSurround);
+        cursor.setMark(CursorService.LAST_CHANGE_END, endSurround);
+        
         if (indentOperation != null) {
             indentOperation.execute(editorAdaptor, Command.NO_COUNT_GIVEN, selection);
         }
-        // TODO: Put delimiters on line before and after line-wise selection
-        new ChangeDelimiterCommand(new NotYetDelimitedTextObject(selection), delimiters)
-                .execute(editorAdaptor);
+
+        //Create new selection, offsets might have shifted due to indentation.
+        selection = new SimpleSelection(
+                new StartEndTextRange(cursor.getMark(CursorService.LAST_CHANGE_START),
+                                      cursor.getMark(CursorService.LAST_CHANGE_END)));
+        ChangeDelimiterCommand.doIt(editorAdaptor, 0, new NotYetDelimitedTextObject(selection),
+                replacement);
     }
 
     private void surroundLinesInBlock(EditorAdaptor editorAdaptor,
@@ -54,15 +114,20 @@ public class AddDelimiterToSelectionOperation implements TextOperation {
         TextBlock textBlock = BlockWiseSelection.getTextBlock(range.getStart(), range.getEnd(),
                 editorAdaptor.getModelContent(), editorAdaptor.getCursorService());
         // TODO: Surround each block item
-        new ChangeDelimiterCommand(new NotYetDelimitedTextObject(selection), delimiters)
+        new ChangeDelimiterCommand(new NotYetDelimitedTextObject(selection), replacement)
                 .execute(editorAdaptor);
     }
 
     /**
-     * Not supported (for now), use <tt>gv</tt> to restore the selection and re-enter this command.
+     * Not supported (for now).
      */
     @Override
     public TextOperation repetition() {
         return null;
+    }
+
+    @Override
+    public void delimiterChanged(DelimiterHolder from, DelimiterHolder to) {
+        this.replacement = to;
     }
 }
