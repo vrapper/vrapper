878fea8bd20637f3ba944a43eb307b7d02e13d92
albertdev <albert.devs@gmail.com>
2013-08-10 12:54:15 +0200

    S, cc, Vs and Vc change last line correctly
    As reported in issue #272 , the last line would be deleted and the
    cursor moved up to a new line above the second-last line.
    
    This commit fixes it by detecting the last line case and inserting a new
    line AFTER the deleted line.

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/ChangeOperation.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/ChangeOperation.java
@@ -1,9 +1,11 @@
 package net.sourceforge.vrapper.vim.commands;
 
 import static net.sourceforge.vrapper.vim.commands.ConstructorWrappers.seq;
+import net.sourceforge.vrapper.platform.TextContent;
 import net.sourceforge.vrapper.utils.ContentType;
 import net.sourceforge.vrapper.utils.SelectionArea;
 import net.sourceforge.vrapper.utils.TextRange;
+import net.sourceforge.vrapper.utils.VimUtils;
 import net.sourceforge.vrapper.vim.EditorAdaptor;
 import net.sourceforge.vrapper.vim.modes.ExecuteCommandHint;
 import net.sourceforge.vrapper.vim.modes.InsertMode;
@@ -55,9 +57,23 @@ public class ChangeOperation implements TextOperation {
         }
     }
 
-    static Command getHintCommand(final EditorAdaptor editorAdaptor, final int count, final TextObject textObject) {
+    static Command getHintCommand(final EditorAdaptor editorAdaptor, final int count, final TextObject textObject) throws CommandExecutionException {
         Command result = new TextOperationTextObjectCommand(DeleteOperation.INSTANCE, textObject).withCount(count);
-        if (ContentType.LINES.equals(textObject.getContentType(editorAdaptor.getConfiguration()))) {
+
+        //if we're in LINES mode and on the last line, delete and then recreate that last line.
+        TextRange region = textObject.getRegion(editorAdaptor, count);
+        ContentType contentType = textObject.getContentType(editorAdaptor.getConfiguration());
+        TextContent txtContent = editorAdaptor.getModelContent();
+        int position = region.getLeftBound().getModelOffset();
+        int length = region.getModelLength();
+        String text = txtContent.getText(position, length);
+
+        if (contentType == ContentType.LINES && position > 0
+                && (text.length() == 0 || ! VimUtils.isNewLine(text.substring(text.length()-1)))) {
+            result = seq(result, InsertLineCommand.POST_CURSOR);
+
+        //if we're in LINES mode but somewhere else in the file
+        } else if (ContentType.LINES.equals(contentType)) {
             result = seq(result, InsertLineCommand.PRE_CURSOR);
         }
         return result;
