950aab08237ddc200457a6796fcb3f2a54d0b05b
albertdev <albert.devs@gmail.com>
2014-03-23 22:46:08 +0100

    Document all Selection methods.
    Especially getFrom / getTo are confusing.

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/Selection.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/Selection.java
@@ -5,10 +5,35 @@ import net.sourceforge.vrapper.utils.TextRange;
 import net.sourceforge.vrapper.vim.EditorAdaptor;
 
 public interface Selection extends TextObject, TextRange {
+    /** Whether the selection is from bottom to top and / or right to left. */
     public boolean isReversed();
+    /**
+     * Returns a character near the start boundary of the selection.
+     * 
+     * <p>The difference between {@link #getStart()} is subtle to none. It can only be really seen
+     * when a {@link LineWiseSelection} is active. In that case, {@link #getFrom()} returns
+     * any character on the starting line, whereas {@link #getStart()} will always return the first
+     * or last character on that line, depending on whether the selection is reversed or not.
+     */
     public Position getFrom();
+    /**
+     * Returns the position of the "cursor" near the end of the selection. Vim allows to show this
+     * "cursor" in line-wise mode but Eclipse can't do it because the caret must always be at either
+     * end of the selection.
+     * 
+     * <p>The difference between {@link #getEnd()} is subtle to none. It can only be really seen
+     * when a {@link LineWiseSelection} is active. In that case, {@link #getTo()} returns
+     * the position of the "cursor" on the ending line, whereas {@link #getEnd()} will always return
+     * the first or last character on that line, depending on whether the selection is reversed or
+     * not.
+     */
     public Position getTo();
+    /** Returns the left- and top-most position included in the selection, even if reversed. */
     public Position getStartMark(EditorAdaptor defaultEditorAdaptor);
+    /**
+     * Returns the right- and bottom-most position included in the selection, even if reversed.
+     * <p>For line-wise selection, this doesn't include the final newline.
+     */
     public Position getEndMark(EditorAdaptor defaultEditorAdaptor);
     /** Returns the name of the mode in which this selection was made. */
     public String getModeName();
