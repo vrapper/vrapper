195f43c15552a70a2d7b1c51b6896c156c6adb6d
albertdev <albert.devs@gmail.com>
2014-06-09 22:00:57 +0200

    Change website's documentation URIs.
    Currently those URIs are written as documentation/index.php?page=4 which
    isn't that informative when the URI is bookmarked.
    
    Redirect the user to e.g. index.php?topic=commandline_commands instead.

--- website/documentation/index.php
+++ website/documentation/index.php
@@ -12,19 +12,27 @@ $docs = array(
     "marks_and_registers",
     "optional_plugins"
 );
-if (isset($_GET['page'])) {
-    $subpage = $_GET['page'];
+if (isset($_GET['page']) && $_GET['page'] > 0 && $_GET['page'] <= count($docs)) {
+    # Send "Moved permanently to new URL", hopefully user changes bookmarks.
+    header("Location: index.php?topic=" . $docs[$_GET['page'] - 1], true, 301);
+    exit;
+} else if (isset($_GET['topic'])) {
+    $subpageId = array_search($_GET['topic'], $docs);
+    if ($subpageId === FALSE) {
+        http_response_code(404);
+        exit;
+    }
 } else {
-    $subpage = 1;
+    $subpageId = 0;
 }
 include("../includes/header.html");
 ?>
 <ol id="index">
-<?php for($i = 1; $i <= sizeof($docs); $i++) { ?>
-    <li<?php if($i == $subpage){echo ' class="selected" ';}?>><a href="?page=<?php echo $i ?>"><?php echo str_replace('_', ' ', $docs[$i-1]) ?></a></li>
+<?php for($i = 0; $i < sizeof($docs); $i++) { ?>
+    <li<?php if($i == $subpageId){echo ' class="selected" ';}?>><a href="?topic=<?php echo $docs[$i] ?>"><?php echo str_replace('_', ' ', $docs[$i]) ?></a></li>
 <?php } ?>
 </ol>
 <div id="documentation">
-<?php include($docs[$subpage-1].".html"); ?>
+<?php include($docs[$subpageId].".html"); ?>
 </div>
 <?php include("../includes/footer.html"); ?>
