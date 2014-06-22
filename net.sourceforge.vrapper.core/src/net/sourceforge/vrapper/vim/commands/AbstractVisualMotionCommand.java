f83ca3d7fd89fd834d1e8dc35f6fa0085a776c25
albertdev <albert.devs@gmail.com>
2014-06-22 18:04:53 +0200

    Prevent screen flicker during selection changes.
    Since the selection change code first moves the caret and then restores
    the selection, changing the selection with "line down" would continously
    flash the screen.
    
    Disabling redraw during such a change allows for a smoother experience.

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/AbstractVisualMotionCommand.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/AbstractVisualMotionCommand.java
@@ -1,5 +1,6 @@
 package net.sourceforge.vrapper.vim.commands;
 
+import net.sourceforge.vrapper.platform.ViewportService;
 import net.sourceforge.vrapper.vim.EditorAdaptor;
 import net.sourceforge.vrapper.vim.commands.motions.Motion;
 import net.sourceforge.vrapper.vim.commands.motions.StickyColumnPolicy;
@@ -18,10 +19,21 @@ public abstract class AbstractVisualMotionCommand extends MotionCommand {
     @Override
     public void execute(EditorAdaptor editorAdaptor, int count)
             throws CommandExecutionException {
-            	Selection oldSelection = getSelection(editorAdaptor);
-            	editorAdaptor.setPosition(oldSelection.getTo(), StickyColumnPolicy.NEVER);
-            	super.execute(editorAdaptor, count);
-            	extendSelection(editorAdaptor, oldSelection, count);
-            }
+        Object redrawLock = new Object();
+        ViewportService viewportService = editorAdaptor.getViewportService();
+        try {
+            viewportService.setRepaint(false);
+            viewportService.lockRepaint(redrawLock);
+
+            Selection oldSelection = getSelection(editorAdaptor);
+            editorAdaptor.setPosition(oldSelection.getTo(), StickyColumnPolicy.NEVER);
+            super.execute(editorAdaptor, count);
+            extendSelection(editorAdaptor, oldSelection, count);
+
+        } finally {
+            viewportService.unlockRepaint(redrawLock);
+            viewportService.setRepaint(true);
+        }
+    }
 
 }
