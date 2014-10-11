175f07c1975c16eb50359c9357fd3dd7dd0f3ca1
albertdev <albert.devs@gmail.com>
2014-10-11 15:03:17 +0200

    Documentation

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/motions/MoveRightAcrossLines.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/motions/MoveRightAcrossLines.java
@@ -3,6 +3,10 @@ package net.sourceforge.vrapper.vim.commands.motions;
 import net.sourceforge.vrapper.platform.TextContent;
 import net.sourceforge.vrapper.utils.LineInformation;
 
+/**
+ * Moves the caret to the right or next line. Differs between normal and visual mode, the latter
+ * should use {@link #INSTANCE_BEHIND_CHAR}.
+ */
 public class MoveRightAcrossLines extends LeftRightMotion {
 
     public static final Motion INSTANCE = new MoveRightAcrossLines(true);
@@ -19,6 +23,8 @@ public class MoveRightAcrossLines extends LeftRightMotion {
 		int len = content.getTextLength();
 		int result = Math.min(len, offset+count);
 		LineInformation lineInformation = content.getLineInformationOfOffset(result);
+		// If moving into a newline combo, move to next line. If we're in normal mode
+		// (skipLastChar=true), immediately move to next line if caret is on last character of line.
 		if (result < len
 		        && (result > lineInformation.getEndOffset()
 		                || (skipLastChar && result == lineInformation.getEndOffset()))) {
