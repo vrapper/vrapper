101e71c3d0e50af7ea3421f5e99a1b5842b30785
albertdev <albert.devs@gmail.com>
2014-10-20 17:25:17 +0200

    Rework status widget to use styling, be smaller.
    Use a custom widget extending from CLabel to allow styling to kick in
    while keeping the ability to do custom text shortening (Eclipse by
    default replaces the middle of the label with an ellipsis).
    
    The old widget was also using a fixed-width layout to stop the
    "recording" text from being shortened / hidden, but this has the result
    that other status items might be squeezed. We now use 3 widgets: a
    seperator, the mode widget and the "recording" widget, of which the last
    one is not always added to the status bar.
    
    Fixes #499, alternative fix for #322 and possible workaround for #357.

--- net.sourceforge.vrapper.eclipse/src/net/sourceforge/vrapper/eclipse/ui/ModeContributionItem.java
+++ net.sourceforge.vrapper.eclipse/src/net/sourceforge/vrapper/eclipse/ui/ModeContributionItem.java
@@ -1,72 +1,143 @@
 package net.sourceforge.vrapper.eclipse.ui;
 
+import java.util.Locale;
+
+import net.sourceforge.vrapper.platform.UserInterfaceService;
+
 import org.eclipse.jface.action.ContributionItem;
+import org.eclipse.jface.action.StatusLineLayoutData;
+import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
-import org.eclipse.swt.graphics.FontData;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
 public class ModeContributionItem extends ContributionItem {
-    
+
+    private static final String DEFAULT_MESSAGE = UserInterfaceService.VRAPPER_DISABLED;
     private static final String RECORDING_MESSAGE = "recording: ";
-    private static final int MODEMESSAGE_WIDTH = 50;
+    // Disabled message is treated as longest possible string.
+    private static final int LABEL_MARGIN = 3;
     private String mode = "";
     private boolean isRecording;
     private String recMacro;
-    
+    private CLabel recordingText;
+    private CLabel modeText;
+
     public ModeContributionItem(String id) {
         super(id);
     }
 
+    //XXX Extending an SWT Widget is not recommended but we want custom text shortening.
+    //This might break CSS theme support in E4 as we can't use the E4 workarounds until we drop 3.x.
+    protected static class ModeLabel extends CLabel {
+
+        private String modeName;
+
+        public ModeLabel(Composite parent, int style) {
+            super(parent, style);
+        }
+
+        @Override
+        protected String shortenText(GC gc, String t, int width) {
+            // Drop the "--" at the ends if the current text is too long.
+            String shorter = modeName;
+            // If set to e.g. "(insert) VISUAL LINE", make it even shorter.
+            if (gc.textExtent(shorter).x > width
+                    && shorter.toUpperCase(Locale.ENGLISH).contains("VISUAL")) {
+                shorter = shorter.replace("VISUAL", "VIS");
+            }
+            return shorter;
+        }
+
+        public void setText(String modeName) {
+            this.modeName = modeName;
+            super.setText("-- " + modeName + " --");
+        }
+    }
+
     @Override
     public void fill(Composite parent) {
-        Composite reservedSpace = new Composite(parent, SWT.NO_FOCUS);
-        reservedSpace.setBackground(parent.getBackground());
-        reservedSpace.setForeground(parent.getForeground());
-        GridLayout layout = new GridLayout();
-        layout.horizontalSpacing = 50;
-        layout.numColumns = 2;
-        reservedSpace.setLayout(layout);
-        
-        Label recordingText = new Label(reservedSpace, SWT.CENTER);
-        recordingText.setText(RECORDING_MESSAGE + recMacro);
-        recordingText.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
-        recordingText.setBackground(parent.getBackground());
-        recordingText.setForeground(parent.getForeground());
-        recordingText.setVisible(isRecording);
-        Font boldFont = recordingText.getFont();
-        FontData fontData[] = boldFont.getFontData();
-        for (FontData fd : fontData) {
-            fd.setStyle(SWT.BOLD);
+
+        Color background = parent.getBackground();
+        Color foreground = parent.getForeground();
+        Font boldFont = JFaceResources.getFontRegistry().getBold("");
+        Font regularFont = JFaceResources.getFontRegistry().defaultFont();
+
+        // Calculate height and width based on current above fonts.
+        int fontHeight;
+        int modeLabelWidth;
+        int recordingLabelWidth;
+        GC gc = null;
+        try {
+            gc = new GC(parent);
+            gc.setFont(regularFont);
+            fontHeight = gc.getFontMetrics().getHeight();
+            modeLabelWidth = gc.textExtent(DEFAULT_MESSAGE).x + LABEL_MARGIN * 2;
+            gc.setFont(boldFont);
+            recordingLabelWidth = gc.textExtent(RECORDING_MESSAGE + "@").x + LABEL_MARGIN * 2;
+        } finally {
+            if (gc != null) {
+                gc.dispose();
+            }
         }
-        recordingText.setFont(new Font(boldFont.getDevice(), fontData));
-        
-        Label modeText = new Label(reservedSpace, SWT.NONE);
-        GridData modeLayoutData = new GridData(SWT.RIGHT, SWT.CENTER, true, false);
-        modeLayoutData.minimumWidth = MODEMESSAGE_WIDTH;
-        modeText.setLayoutData(modeLayoutData);
-        modeText.setBackground(parent.getBackground());
-        modeText.setForeground(parent.getForeground());
+
+        StatusLineLayoutData layoutData;
+
+        //Note: attentive readers might remark that no dispose is called on the following widgets.
+        // The status bar manager actually disposes all child widgets of the parent Composite before
+        // calling this fill method again.
+
+        Label sep = new Label(parent, SWT.SEPARATOR);
+        layoutData = new StatusLineLayoutData();
+        layoutData.heightHint = fontHeight;
+        sep.setLayoutData(layoutData);
+
+        if (isRecording) {
+            recordingText = new CLabel(parent, SWT.SHADOW_NONE | SWT.CENTER);
+            recordingText.setText(getRecordingLabelText());
+            recordingText.setBackground(background);
+            recordingText.setForeground(foreground);
+            recordingText.setVisible(isRecording);
+            recordingText.setFont(boldFont);
+            layoutData = new StatusLineLayoutData();
+            layoutData.widthHint = recordingLabelWidth;
+            recordingText.setLayoutData(layoutData);
+        }
+        modeText = new ModeLabel(parent, SWT.SHADOW_NONE | SWT.CENTER);
+        modeText.setBackground(background);
+        modeText.setForeground(foreground);
         modeText.setText(mode);
-        
-        reservedSpace.layout();
+        modeText.setFont(regularFont);
+        StatusLineLayoutData modeLayoutData = new StatusLineLayoutData();
+        modeLayoutData.widthHint = modeLabelWidth;
+        modeText.setLayoutData(modeLayoutData);
     }
-    
+
+    private String getRecordingLabelText() {
+        return RECORDING_MESSAGE + recMacro;
+    }
+
     public void setText(String text) {
         mode = text;
-        if (super.getParent() != null) {
-            super.getParent().update(true);
+        if (modeText != null && ! modeText.isDisposed()) {
+            modeText.setText(text);
         }
     }
 
     public void setRecording(boolean b, String m) {
         isRecording = b;
         recMacro = m;
-        if (super.getParent() != null) {
-            super.getParent().update(true);
+        if (recordingText != null && ! recordingText.isDisposed()) {
+            recordingText.setText(getRecordingLabelText());
+            recordingText.setVisible(isRecording);
+        }
+        if (getParent() != null) {
+            // Add or remove recording label widget by forcing status bar refresh.
+            getParent().update(true);
         }
     }
 }
