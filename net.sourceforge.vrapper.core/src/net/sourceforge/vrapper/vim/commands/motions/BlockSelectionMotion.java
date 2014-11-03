80740a1208948ba970c0222838f0634728a32fe1
igrekster <igrek+github@fastem.com>
2014-11-03 20:40:43 +1100

    Fix #545: Move cursor to EOL for $-based text block append (`A`)

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/motions/BlockSelectionMotion.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/motions/BlockSelectionMotion.java
@@ -2,6 +2,7 @@ package net.sourceforge.vrapper.vim.commands.motions;
 
 import net.sourceforge.vrapper.platform.CursorService;
 import net.sourceforge.vrapper.platform.TextContent;
+import net.sourceforge.vrapper.utils.BlockWiseSelectionArea;
 import net.sourceforge.vrapper.utils.LineInformation;
 import net.sourceforge.vrapper.utils.Position;
 import net.sourceforge.vrapper.utils.SelectionArea;
@@ -46,7 +47,8 @@ public class BlockSelectionMotion implements Motion {
         if (selection == null)
             throw new CommandExecutionException("BlockSelectionMotion must have a selection!");
         
-        final SelectionArea lastSel = editorAdaptor.getRegisterManager().getLastActiveSelectionArea();
+        final BlockWiseSelectionArea lastSel = (BlockWiseSelectionArea)
+                editorAdaptor.getRegisterManager().getLastActiveSelectionArea();
         final CursorService cs = editorAdaptor.getCursorService();
         final TextContent mc = editorAdaptor.getModelContent();
         final TextRange tr = lastSel.getRegion(editorAdaptor, NO_COUNT_GIVEN);
@@ -54,7 +56,10 @@ public class BlockSelectionMotion implements Motion {
         if (gotoStart) {
             return cs.getPositionByVisualOffset(tb.startLine, tb.startVisualOffset);
         } else {
-            final Position pos = cs.getPositionByVisualOffset(tb.startLine, tb.endVisualOffset);
+            Position pos = null;
+            if (!lastSel.isUntilEOL()) {
+                pos = cs.getPositionByVisualOffset(tb.startLine, tb.endVisualOffset);
+            }
             final LineInformation line = mc.getLineInformation(tb.startLine);
             if (pos != null && pos.getModelOffset() < line.getEndOffset()) {
                 return pos.addModelOffset(1);
