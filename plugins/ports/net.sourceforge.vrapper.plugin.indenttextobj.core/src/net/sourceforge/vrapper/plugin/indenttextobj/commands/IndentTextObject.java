5f2799d3ace08354d87f0f9a3a9bdc0f459713cf
keforbes <keforbes@gmail.com>
2015-02-06 23:54:56 -0700

    Play with #591.  Attempt to create an optional plugin for indent text objects.  ai/ii seems to work fine but aI/iI doesn't do anything.  I don't know why 'I' is being ignored.

---
+++ plugins/ports/net.sourceforge.vrapper.plugin.indenttextobj.core/src/net/sourceforge/vrapper/plugin/indenttextobj/commands/IndentTextObject.java
@@ -0,0 +1,85 @@
+package net.sourceforge.vrapper.plugin.indenttextobj.commands;
+
+import net.sourceforge.vrapper.platform.Configuration;
+import net.sourceforge.vrapper.platform.CursorService;
+import net.sourceforge.vrapper.platform.TextContent;
+import net.sourceforge.vrapper.utils.ContentType;
+import net.sourceforge.vrapper.utils.LineInformation;
+import net.sourceforge.vrapper.utils.Position;
+import net.sourceforge.vrapper.utils.StartEndTextRange;
+import net.sourceforge.vrapper.utils.TextRange;
+import net.sourceforge.vrapper.utils.VimUtils;
+import net.sourceforge.vrapper.vim.EditorAdaptor;
+import net.sourceforge.vrapper.vim.commands.AbstractTextObject;
+import net.sourceforge.vrapper.vim.commands.CommandExecutionException;
+import net.sourceforge.vrapper.vim.commands.TextObject;
+
+public class IndentTextObject extends AbstractTextObject {
+
+    public static final TextObject INNER_INNER = new IndentTextObject(false, false);
+    public static final TextObject OUTER_INNER = new IndentTextObject(true, false);
+    public static final TextObject INNER_OUTER = new IndentTextObject(false, true);
+    public static final TextObject OUTER_OUTER = new IndentTextObject(true, true);
+
+    private final boolean includeFirstLine;
+    private final boolean includeLastLine;
+
+    private IndentTextObject(boolean firstLine, boolean lastLine) {
+        this.includeFirstLine = firstLine;
+        this.includeLastLine = lastLine;
+    }
+
+    public ContentType getContentType(Configuration configuration) {
+        return ContentType.LINES;
+    }
+
+    @Override
+    public TextRange getRegion(EditorAdaptor editorAdaptor, int count) throws CommandExecutionException {
+    	TextContent model = editorAdaptor.getModelContent();
+    	LineInformation cursorLine = model.getLineInformationOfOffset(editorAdaptor.getPosition().getModelOffset());
+    	int indentLength = VimUtils.getIndent(model, cursorLine).length();
+
+    	//if no other lines match, default to current line
+    	int start = cursorLine.getBeginOffset();
+    	int end = cursorLine.getEndOffset();
+
+    	int lineNo = cursorLine.getNumber() - 1;
+    	LineInformation previousLine = cursorLine;
+    	LineInformation line;
+    	
+    	//find previous line with different indent
+    	while(lineNo >= 0) {
+    		line = model.getLineInformation(lineNo);
+    		if( (! VimUtils.isLineBlank(model, lineNo)) &&
+    				VimUtils.getIndent(model, line).length() < indentLength) {
+    			start = includeFirstLine ? line.getBeginOffset() : previousLine.getBeginOffset();
+    			break;
+    		}
+    		previousLine = line;
+    		lineNo--;
+    	}
+
+    	//reset for next iteration
+    	lineNo = cursorLine.getNumber() + 1;
+    	previousLine = cursorLine;
+    	int total = model.getNumberOfLines();
+    	
+    	//find next line with different indent
+    	while(lineNo < total) {
+    		line = model.getLineInformation(lineNo);
+    		if( (! VimUtils.isLineBlank(model, lineNo)) &&
+    				VimUtils.getIndent(model, line).length() < indentLength) {
+    			end = includeLastLine ? line.getEndOffset() : previousLine.getEndOffset();
+    			break;
+    		}
+    		previousLine = line;
+    		lineNo++;
+    	}
+
+    	CursorService cursor = editorAdaptor.getCursorService();
+    	Position startPos = VimUtils.fixLeftDelimiter(model, cursor, cursor.newPositionForModelOffset(start));
+    	Position endPos = VimUtils.fixRightDelimiter(model, cursor, cursor.newPositionForModelOffset(end));
+    	return new StartEndTextRange(startPos, endPos);
+    }
+
+}
---
+++ plugins/ports/net.sourceforge.vrapper.plugin.indenttextobj.core/src/net/sourceforge/vrapper/plugin/indenttextobj/platform/IndentTextObjectProvider.java
@@ -0,0 +1,36 @@
+package net.sourceforge.vrapper.plugin.indenttextobj.platform;
+
+import static net.sourceforge.vrapper.keymap.vim.ConstructorWrappers.leafBind;
+import static net.sourceforge.vrapper.keymap.vim.ConstructorWrappers.state;
+import static net.sourceforge.vrapper.keymap.vim.ConstructorWrappers.transitionBind;
+import net.sourceforge.vrapper.keymap.EmptyState;
+import net.sourceforge.vrapper.keymap.State;
+import net.sourceforge.vrapper.platform.AbstractPlatformSpecificTextObjectProvider;
+import net.sourceforge.vrapper.plugin.indenttextobj.commands.IndentTextObject;
+import net.sourceforge.vrapper.vim.commands.DelimitedText;
+import net.sourceforge.vrapper.vim.commands.TextObject;
+
+public class IndentTextObjectProvider extends
+        AbstractPlatformSpecificTextObjectProvider {
+
+    @Override
+    public String getName() {
+        return "IndentTextObject";
+    }
+
+    @Override
+    public State<DelimitedText> delimitedTexts() {
+        return EmptyState.getInstance();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public State<TextObject> textObjects() {
+        return state(
+                transitionBind('i', leafBind('i', IndentTextObject.INNER_INNER)),
+                transitionBind('a', leafBind('i', IndentTextObject.OUTER_INNER)),
+                transitionBind('i', leafBind('I', IndentTextObject.INNER_OUTER)),
+                transitionBind('a', leafBind('I', IndentTextObject.OUTER_OUTER)));
+    }
+
+}
