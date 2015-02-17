ffc707d7533042a741044a30e8a69dc3f7dd4974
albertdev <albert.devs@gmail.com>
2015-02-17 21:36:13 +0100

    Javadoc for BufferManager

--- net.sourceforge.vrapper.eclipse/src/net/sourceforge/vrapper/eclipse/interceptor/BufferManager.java
+++ net.sourceforge.vrapper.eclipse/src/net/sourceforge/vrapper/eclipse/interceptor/BufferManager.java
@@ -13,10 +13,30 @@ import org.eclipse.ui.IEditorReference;
  */
 public interface BufferManager {
     
+    /** Reserves a buffer id for the given {@link IEditorReference}. */
     public void registerEditorRef(IEditorReference ref);
+
+    /**
+     * Reserves a buffer id for the given {@link IEditorPart}. It will try to reuse the buffer id of
+     * any registered {@link IEditorReference}. If the editor is a multi-page editor, buffer ids are
+     * reserved for those inner editors.
+     */
     public void registerEditorPart(NestedEditorPartInfo nestingInfo, IEditorPart editorPart,
             boolean updateLastSeen);
+
+    /** Find buffer information about the last editor which has shown given file input. */
     public BufferInfo getBuffer(IEditorInput editorInput);
+
+    /**
+     * Retrieve information about all known buffers. As noted on other methods, this includes
+     * editors from the last Eclipse session and files which are no longer opened.
+     * <p>Users should be aware that buffers for all windows are returned.
+     */
     public List<BufferInfo> getBuffers();
+
+    /**
+     * Activate the editor in which the given buffer (file info) was last opened. This editor opens
+     * in the currently active window.
+     */
     public void activate(BufferInfo buffer);
 }
