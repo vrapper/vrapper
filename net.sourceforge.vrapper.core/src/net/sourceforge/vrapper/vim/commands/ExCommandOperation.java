63b0cc47bc7579e87298025bb8ba0060dc75bfbc
albertdev <albert.devs@gmail.com>
2015-07-01 22:49:04 +0200

    Sanitize whitespace, try/finally around history lock
    Use tabs everywhere, clear empty lines.

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/ExCommandOperation.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/ExCommandOperation.java
@@ -21,9 +21,9 @@ import net.sourceforge.vrapper.vim.EditorAdaptor;
  * We take those pieces and generate a Command if everything is valid.
  */
 public class ExCommandOperation extends SimpleTextOperation {
-	
+
 	String originalDefinition;
-	
+
 	public ExCommandOperation(String definition) {
 		this.originalDefinition = definition;
 	}
@@ -42,12 +42,12 @@ public class ExCommandOperation extends SimpleTextOperation {
 		}
 	}
 
-    public void execute(EditorAdaptor editorAdaptor, TextRange region, ContentType contentType)
-            throws CommandExecutionException {
-    	boolean findMatch = true;
-    	//leave 'originalDefinition' untouched so repetition can use it again
-    	//we'll be modifying 'definition' to make parsing easier
-    	String definition = originalDefinition;
+	public void execute(EditorAdaptor editorAdaptor, TextRange region, ContentType contentType)
+			throws CommandExecutionException {
+		boolean findMatch = true;
+		//leave 'originalDefinition' untouched so repetition can use it again
+		//we'll be modifying 'definition' to make parsing easier
+		String definition = originalDefinition;
 		if(definition.startsWith("g!")) {
 			findMatch = false;
 			//chop off 'g!'
@@ -67,21 +67,21 @@ public class ExCommandOperation extends SimpleTextOperation {
 			VrapperLog.error("Expected a g or v instruction but got [" + originalDefinition + "]");
 			throw new CommandExecutionException("Parsing error");
 		}
-		
+
 		//a search pattern must be defined but it doesn't have to be '/'
 		//whatever character is after 'g', 'g!', or 'v' must be pattern delimiter
 		char delimiter = definition.charAt(0);
-		
+
 		//delimiter is at 1, where is it's match?
 		int patternEnd = definition.indexOf(delimiter, 1);
 		if(patternEnd == -1) {
 			//pattern didn't end
 			throw new CommandExecutionException("Missing separator!");
 		}
-		
+
 		//grab text between delimiters
 		String pattern = definition.substring(1, patternEnd);
-		
+
 		if (pattern.length() == 0) {
 			// If no pattern defined, use last search.
 			// Register manager guarantees that the register content is not null here.
@@ -95,17 +95,17 @@ public class ExCommandOperation extends SimpleTextOperation {
 			// pattern was defined, but no command
 			throw new CommandExecutionException("No ex command to execute on pattern match!");
 		}
-		
+
 		//chop off pattern (+delimiter), all that should be left is command
 		definition = definition.substring(patternEnd + 1);
-		
+
 		SimpleTextOperation operation = buildExCommand(definition, editorAdaptor);
-		
+
 		if(operation != null) {
 			executeExCommand(region, findMatch, pattern, operation, editorAdaptor);
 		}
 	}
-	
+
 	private SimpleTextOperation buildExCommand(String command, EditorAdaptor editorAdaptor) {
 		if(command.startsWith("normal ")) {
 			String args = command.substring("normal ".length());
@@ -118,64 +118,67 @@ public class ExCommandOperation extends SimpleTextOperation {
 			return DeleteOperation.INSTANCE;
 		}
 		else if(command.startsWith("y")) {
-    	    if (command.length() >= 3) {
-    	        return new YankOperation(command.substring(command.length() - 1));
-    	    } else {
-    	        return YankOperation.INSTANCE;
-    	    }
+			if (command.length() >= 3) {
+				return new YankOperation(command.substring(command.length() - 1));
+			} else {
+				return YankOperation.INSTANCE;
+			}
 		}
-		
+
 		return null;
 	}
-	
+
 	private void executeExCommand(TextRange region, boolean findMatch,
 			String pattern, SimpleTextOperation operation, EditorAdaptor editorAdaptor) {
-		
-    	int startLine;
-    	int endLine;
-    	if(region == null) { //default case, entire file
-    		startLine = 0;
-    		endLine = editorAdaptor.getModelContent().getNumberOfLines();
-    	}
-    	else {
-	    	startLine = editorAdaptor.getModelContent()
-	    			.getLineInformationOfOffset( region.getLeftBound().getModelOffset() ).getNumber();
-	    	endLine = editorAdaptor.getModelContent()
-	    			.getLineInformationOfOffset( region.getRightBound().getModelOffset() ).getNumber();
-    	}
-		
+
+		int startLine;
+		int endLine;
+		if (region == null) { //default case, entire file
+			startLine = 0;
+			endLine = editorAdaptor.getModelContent().getNumberOfLines();
+		}
+		else {
+			startLine = editorAdaptor.getModelContent()
+					.getLineInformationOfOffset( region.getLeftBound().getModelOffset() ).getNumber();
+			endLine = editorAdaptor.getModelContent()
+					.getLineInformationOfOffset( region.getRightBound().getModelOffset() ).getNumber();
+		}
+
 		LineInformation line;
 		editorAdaptor.getHistory().beginCompoundChange();
 		editorAdaptor.getHistory().lock("ex-command");
-		if(startLine == endLine) {
-			line = editorAdaptor.getModelContent().getLineInformation(startLine);
-			processLine(pattern, findMatch, operation, line, editorAdaptor);
-		}
-		else {
-			int oldLineCount = editorAdaptor.getModelContent().getNumberOfLines();
-			for(int i=startLine; i < endLine; i++) {
-				line = editorAdaptor.getModelContent().getLineInformation(i);
-				boolean operationPerformed = processLine(pattern, findMatch, operation, line, editorAdaptor);
-				
-				if(operationPerformed) {
-					int currentNumLines = editorAdaptor.getModelContent().getNumberOfLines();
-					//if this was a destructive operation and a line was removed
-					//stay in sync
-					if(oldLineCount > currentNumLines) {
-						//next line moved up, make sure we don't skip it
-						i--;
-						//make sure we don't run outside our boundary
-						endLine--;
-						oldLineCount = currentNumLines;
+		try {
+			if (startLine == endLine) {
+				line = editorAdaptor.getModelContent().getLineInformation(startLine);
+				processLine(pattern, findMatch, operation, line, editorAdaptor);
+			}
+			else {
+				int oldLineCount = editorAdaptor.getModelContent().getNumberOfLines();
+				for (int i=startLine; i < endLine; i++) {
+					line = editorAdaptor.getModelContent().getLineInformation(i);
+					boolean operationPerformed = processLine(pattern, findMatch, operation, line, editorAdaptor);
+
+					if (operationPerformed) {
+						int currentNumLines = editorAdaptor.getModelContent().getNumberOfLines();
+						//if this was a destructive operation and a line was removed
+						//stay in sync
+						if (oldLineCount > currentNumLines) {
+							//next line moved up, make sure we don't skip it
+							i--;
+							//make sure we don't run outside our boundary
+							endLine--;
+							oldLineCount = currentNumLines;
+						}
 					}
 				}
 			}
+		} finally {
+			editorAdaptor.getHistory().unlock("ex-command");
+			editorAdaptor.getHistory().endCompoundChange();
 		}
-		editorAdaptor.getHistory().unlock("ex-command");
-		editorAdaptor.getHistory().endCompoundChange();
-		
+
 	}
-	
+
 	private boolean processLine(String pattern, boolean findMatch, SimpleTextOperation operation,
 			LineInformation line, EditorAdaptor editorAdaptor) {
 		boolean operationPerformed = false;
@@ -191,14 +194,14 @@ public class ExCommandOperation extends SimpleTextOperation {
 			//line can end with anything
 			pattern += ".*";
 		}
-		
+
 		boolean matches = text.matches(pattern);
 		if( (findMatch && matches) || (!findMatch && !matches) ) {
 			try {
 				Position start = editorAdaptor.getCursorService().newPositionForModelOffset(line.getBeginOffset());
 				Position end = editorAdaptor.getCursorService().newPositionForModelOffset(line.getEndOffset());
 				TextRange range = new StartEndTextRange(start, end);
-				
+
 				operation.execute(editorAdaptor, range, ContentType.LINES);
 				operationPerformed = true;
 			} catch (CommandExecutionException e) {
