vrapper/vrapper

commit 5a6507533e931a86f169c483a1a7aea5fb060d5f

Nested macros now have atomic undo action
Locking the undo history is a no-op if the same code block is used
reentrant - the inner invocation will simply unlock the history and
cause the next time through to be a different undo item.

net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/MacroPlayer.java
@@ -1,5 +1,6 @@
 package net.sourceforge.vrapper.vim;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -52,18 +53,19 @@ public class MacroPlayer {
      * (This method is now public rather than package-private just
      *  so we can support the 'normal' command, see ":help normal")
      */
-    public void play() {
+    void play(Deque<String> macroStack) {
         ViewportService view = editorAdaptor.getViewportService();
+        String historyLock = "macroplayback " + macroStack;
         try {
             view.setRepaint(false);
             view.lockRepaint(this);
             editorAdaptor.getHistory().beginCompoundChange();
-            editorAdaptor.getHistory().lock("macroplayback");
+            editorAdaptor.getHistory().lock();
             while (! editorAdaptor.abortRecursion && ! playlist.isEmpty()) {
                 editorAdaptor.handleKeyOffRecord(playlist.poll());
             }
         } finally {
-            editorAdaptor.getHistory().unlock("macroplayback");
+            editorAdaptor.getHistory().unlock(historyLock);
             editorAdaptor.getHistory().endCompoundChange();
             view.unlockRepaint(this);
             view.setRepaint(true);

net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/DefaultEditorAdaptor.java
@@ -457,7 +457,7 @@ public class DefaultEditorAdaptor implements EditorAdaptor {
             // recursively. we need a fresh macro player for that.
             final MacroPlayer player = macroPlayer;
             macroPlayer = null;
-            player.play();
+            player.play(macroStack);
             macroStack.pop();
         }
         return result;

commit 1204b598d5183647605a4b8459da9b6293a3bc3b

Fix history locking bug introduced in 5a6507533e931a86f169c483a1a7
The lock and unlock methods must always be in sync to guarantee a proper
unlock sequence.

It might be interesting to refactor the lock method to return a Lock
object if this ever happens again, the Lock object is then responsible
for guaranteeing a proper unlocking sequence.

net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/MacroPlayer.java
@@ -60,7 +60,7 @@ public class MacroPlayer {
             view.setRepaint(false);
             view.lockRepaint(this);
             editorAdaptor.getHistory().beginCompoundChange();
-            editorAdaptor.getHistory().lock();
+            editorAdaptor.getHistory().lock(historyLock);
             while (! editorAdaptor.abortRecursion && ! playlist.isEmpty()) {
                 editorAdaptor.handleKeyOffRecord(playlist.poll());
             }