ab1993e98e55bea240b6d8e231503df310aaad35
Nahuel Soldevilla <solde9@gmail.com>
2016-03-17 23:58:11 -0300

    Modify Command for printing the current position of the cursor
    Modify PrintOffsetInformation to print Column, Line, Word, and Byte
    the same way Vim does it. Create a separate command to print info on
    TextRange: PrintTextRangeInformation.  Prior information present in
    PrintOffsetInformation is maintained in the end of the info message to
    preserve the previous behaviour.
    
    Fix #724

--- net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/PrintOffsetInformation.java
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/PrintOffsetInformation.java
@@ -1,8 +1,16 @@
 package net.sourceforge.vrapper.vim.commands;
 
+import java.io.UnsupportedEncodingException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.sourceforge.vrapper.log.VrapperLog;
+import net.sourceforge.vrapper.platform.CursorService;
+import net.sourceforge.vrapper.platform.TextContent;
 import net.sourceforge.vrapper.platform.UserInterfaceService;
 import net.sourceforge.vrapper.utils.LineInformation;
 import net.sourceforge.vrapper.utils.Position;
+import net.sourceforge.vrapper.utils.VimUtils;
 import net.sourceforge.vrapper.vim.EditorAdaptor;
 
 public class PrintOffsetInformation implements Command {
@@ -29,13 +37,49 @@ public class PrintOffsetInformation implements Command {
         Position position = editorAdaptor.getPosition();
         UserInterfaceService service = editorAdaptor.getUserInterfaceService();
         int visualOffset = editorAdaptor.getCursorService().getVisualOffset(position);
-        service.setInfoSet(true);
-        LineInformation modelLine = editorAdaptor.getModelContent().getLineInformationOfOffset(position.getModelOffset());
+        CursorService cursorService = editorAdaptor.getCursorService();
+        TextContent modelContent = editorAdaptor.getModelContent();
+        int modelOffset = position.getModelOffset();
+        LineInformation modelLine = modelContent.getLineInformationOfOffset(modelOffset);
         LineInformation viewLine = editorAdaptor.getViewContent().getLineInformationOfOffset(position.getViewOffset());
-        service.setLastCommandResultValue("Position M " + position.getModelOffset() + " / "
-        + position.getViewOffset() + " V; line " + modelLine.getNumber() + " / "
-                + viewLine.getNumber() + " view line; horizontal offset " + visualOffset);
-        // TODO Print extra information about selection in case of visual mode.
-        // Better off in a seperate command or sub-mode
+
+        Pattern pattern = Pattern.compile("[\\S]+");
+        int totalWords = 0;
+        String modelText = modelContent.getText();
+        Matcher matcher = pattern.matcher(modelText);
+        String wholeWord = VimUtils.getWordUnderCursor(editorAdaptor, true);
+
+        int wordNumber = 0;
+        String byteOffset = "";
+        while (matcher.find()) {
+            totalWords++;
+            if (matcher.group().equals(wholeWord) && matcher.start() <= modelOffset && modelOffset <= matcher.end()) {
+                wordNumber = totalWords;
+                byteOffset = modelContent.getText(0, position.addModelOffset(1).getModelOffset());
+            }
+            matcher.start();
+        }
+
+        int totalBytes = 0;
+        int byteNumber = 0;
+        try {
+            totalBytes = modelText.getBytes("UTF-8").length;
+            byteNumber = byteOffset.getBytes("UTF-8").length;
+        } catch (UnsupportedEncodingException e) {
+            VrapperLog.info(e.getMessage());
+        }
+
+        service.setInfoSet(true);
+        int colNumber = VimUtils.calculateColForPosition(modelContent, position) + 1;
+        int lineLength = VimUtils.calculateColForPosition(modelContent, cursorService.newPositionForModelOffset(modelLine.getEndOffset()));
+
+        service.setLastCommandResultValue(
+                          "Col " + colNumber + " of " + lineLength + "; "
+                        + "Line " + (modelLine.getNumber() + 1) + " of " + modelContent.getNumberOfLines() + "; "
+                        + "Word " + wordNumber + " of " + totalWords + "; "
+                        + "Byte " + byteNumber + " of " + totalBytes + "; "
+                        + "Position M " + modelOffset + " / " + position.getViewOffset() + " V; "
+                        + "line " + modelLine.getNumber() + " / " + viewLine.getNumber() + " view line; "
+                        + "horizontal offset " + visualOffset);
     }
 }
---
+++ net.sourceforge.vrapper.core/src/net/sourceforge/vrapper/vim/commands/PrintTextRangeInformation.java
@@ -0,0 +1,70 @@
+package net.sourceforge.vrapper.vim.commands;
+
+import java.io.UnsupportedEncodingException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.sourceforge.vrapper.log.VrapperLog;
+import net.sourceforge.vrapper.platform.TextContent;
+import net.sourceforge.vrapper.platform.UserInterfaceService;
+import net.sourceforge.vrapper.utils.TextRange;
+import net.sourceforge.vrapper.vim.EditorAdaptor;
+
+public class PrintTextRangeInformation implements Command {
+
+    public static final Command INSTANCE = new PrintTextRangeInformation();
+
+    @Override
+    public Command withCount(int count) {
+        return this;
+    }
+
+    @Override
+    public int getCount() {
+        return NO_COUNT_GIVEN;
+    }
+
+    @Override
+    public Command repetition() {
+        return null;
+    }
+
+    @Override
+    public void execute(EditorAdaptor editorAdaptor) throws CommandExecutionException {
+        UserInterfaceService service = editorAdaptor.getUserInterfaceService();
+        TextContent modelContent = editorAdaptor.getModelContent();
+
+        Selection range = editorAdaptor.getSelection();
+        TextRange region = range.getRegion(editorAdaptor, NO_COUNT_GIVEN);
+        Pattern pattern = Pattern.compile("[\\S]+");
+
+        String rangeText = modelContent.getText(region);
+        String contentText = modelContent.getText();
+
+        int selectedWords = 0;
+        Matcher matcher = pattern.matcher(rangeText);
+        while (matcher.find()) selectedWords++;
+
+        int totalWords = 0;
+        matcher = pattern.matcher(contentText);
+        while (matcher.find()) totalWords++;
+
+        int selectedLines = modelContent.getNumberOfLines(range);
+        int totalLines = modelContent.getNumberOfLines();
+        
+        int selectedBytes = 0;
+        int totalBytes = 0;
+        try {
+            selectedBytes = rangeText.getBytes("UTF-8").length;
+            totalBytes = contentText.getBytes("UTF-8").length;
+        } catch (UnsupportedEncodingException e) {
+            VrapperLog.info(e.getMessage());
+        }
+
+        service.setInfoSet(true);
+        service.setLastCommandResultValue("Selected " 
+                        + selectedLines + " of " + totalLines + " Lines; "
+                        + selectedWords + " of " + totalWords + " Words; "
+                        + selectedBytes + " of " + totalBytes + " Bytes");
+    }
+}
